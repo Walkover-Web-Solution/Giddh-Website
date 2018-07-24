f2c5b251b9ffeb8ed1e6ed43c5cc22828d1e6271
Nisha Gupta <nishagupta@Nishas-iMac.local>
2018-07-24 17:47:41 +0530

    home page and other page design correction

--- index.php
+++ index.php
@@ -70,60 +70,76 @@
    
   </section> <!-- end of videoBg -->
   <!-- giddh usage section start -->
-  <section class="whitebg homeContent" id="feature">
+  <section class="whitebg homeContent accounting-img" id="homepage">
       <div class="container">
-              <div class="row">
+              <div class="row p-tb-0">
                     <div class="col-sm-12">
-                      <div>
+                      <div class="head">
                       <h1 class="center_heading">Accounting software </h1><span class="center_heading_span"> that’s not just limited to accountants</span>
                       </div>
                   </div>
               </div>
               <div class="row">
-                  <div class="area-flex">
+                  <div class="area-flex col-reverse">
                     <div class="col-sm-5">
-                        <div class="section-left">
+                        <div class="section-left head">
                         <p>Stop logging into your bank account. Your <b>bank syncs</b> with Giddh.</p>
                         </div>
                     </div>
-                    <div class="col-sm-5 col-sm-offset-2">
+                    <div class="col-sm-5 col-sm-offset-2 col-xs-7">
                       <img src="../assets/images/sync-with-bank.svg" alt="Ready your GST with giddh" class="img-responsive">
                     </div>
                   </div>
               </div>
+              <hr class="devider">
               <div class="row">
-                  <div class="area-flex col-reverse">
-                    <div class="col-sm-5">
+                  <div class="area-flex">
+                    <div class="col-sm-5 col-xs-7">
                     <img src="../assets/images/bussiness-report.svg" alt="Access your data with giddh" class="img-responsive">
                     </div>
                     <div class="col-sm-5 col-sm-offset-2">
-                        <div class="section-left">
+                        <div class="section-left head">
                         <p>You are not dependent on your accountant for your <b>business reports</b>.</p>
                         </div>
                     </div>
                   </div>
               </div>
+              <hr class="devider">
               <div class="row">
-                  <div class="area-flex">
+                  <div class="area-flex col-reverse">
                     <div class="col-sm-5">
-                        <div class="section-left">
+                        <div class="section-left head">
                         <p>Every key person in your company can see the <b>numbers relevant</b> to them.</p>
                         </div>
                     </div>
-                    <div class="col-sm-5 col-sm-offset-2">
+                    <div class="col-sm-5 col-sm-offset-2 col-xs-7">
                       <img src="../assets/images/number-relevent.svg" alt="" class="img-responsive">
                     </div>
                   </div>
               </div>
+              <hr class="devider">
               <div class="row">
-                  <div class="area-flex col-reverse">
-                    <div class="col-sm-5">
+                  <div class="area-flex">
+                    <div class="col-sm-5 col-xs-7">
                     <img src="../assets/images/bussiness-inventory.svg" alt="" class="img-responsive">
                     </div>
                     <div class="col-sm-5 col-sm-offset-2">
-                        <div class="section-left">
+                        <div class="section-left head">
                         <p>Bird's eyeview to manage your <b>business inventory</b> efficiently and smartly.</p>
                         </div>
+                    </div>
+                  </div>
+              </div>
+              <hr class="devider">
+              <div class="row p-b-0">
+                  <div class="area-flex col-reverse">
+                    <div class="col-sm-5">
+                        <div class="section-left head">
+                        <p>Have <b>branches?</b> Your accounts are still at the same place.</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-5 col-sm-offset-2 col-xs-7">
+                      <img src="../assets/images/syncewithgiddh.svg" alt="" class="img-responsive">
                     </div>
                   </div>
               </div>
@@ -131,7 +147,7 @@
   </section>
    <!-- giddh usage section end -->
   <!-- start local database -->
-  <section class="p-tb-75 homeBtmhalf gridBg">
+  <section class="homeBtmhalf gridBg">
     <div class="container adjust-width">
       <div class="row text-center">
         <div class="col-sm-8 col-sm-offset-2">
@@ -143,15 +159,15 @@
   </section>
    <!-- end local database -->
   <!-- built for account -->
-  <section class="whitebg homeContent bigthumb" id="feature">
+  <section class="whitebg homeContent bigthumb" id="homepage">
       <div class="container">
-              <div class="row p-b-90">
+              <div class="row">
                     <div class="col-sm-12">
                       <h2 class="center_heading">Built for Accountants</h2><br>
                       <span class="center_sub_heading">Friendly with all other departments Sales, Support, Managment etc...</span>
                   </div>
               </div>
-              <div class="row p-t-30">
+              <div class="row p-t-60">
                       <div class="col-sm-6 col-md-4">
                             <div class="thumbnail feature_thumbnail">
                                 <img src="../assets/images/invoicing.svg" alt="Manage GST with Giddh">
@@ -218,14 +234,14 @@
   </section>  
 <!-- built for account end -->
 <!-- trusted by expert carousel start -->
-<section class="graybg homeContent" id="feature">
+<section class="graybg homeContent" id="homepage">
       <div class="container">
-              <div class="row p-b-0">
+              <div class="row p-tb-0">
                     <div class="col-sm-12">
-                      <h2 class="center_heading">Trusted by the Experts</h2><br>
+                      <h2 class="center_heading">Trusted by Our Customers</h2><br>
                   </div>
               </div>
-              <div class="row">
+              <div class="row m-t-60">
                 <div class="col-sm-10 col-sm-offset-1 testimonial">
                   <ul class="nav nav-testimonial">
                     <li class="card-custom">
@@ -262,7 +278,7 @@
       </div>
   </section>  -->
   <div class="modal fade" id="talkToSales_dialog" role="dialog">
-  <div class="modal-dialog custom-dialog">
+   <div class="modal-dialog custom-dialog">
     <div class="modal-header">
       <h3 class="brdB left">Talk to Expert</h3>
       <a data-dismiss="modal" aria-label="Close" class="close-glyph">✕</a>
@@ -272,7 +288,7 @@
 
        </ul>
     </div>
-       <div class="pd2 tab-content custom-padding-20 height-52 tab-content-modal">
+       <div class="pd2 tab-content custom-padding-20 height-552 tab-content-modal">
 
          <div id="dropEmail" class="tab-pane">
            <div id="Jkvq" class="talktosale-superform"></div>
