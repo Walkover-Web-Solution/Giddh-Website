bad28ca42d92af030e252e9477f5a8cb56793f6b
synramtech <augast@synramtechnolab.com>
2018-10-18 19:20:11 +0530

    New pages created

---
+++ dashboard.php
@@ -0,0 +1,238 @@
+<!DOCTYPE HTML>
+<html lang="en" ng-app="giddhWebsite">
+  <meta charset="UTF-8">
+  <!-- <meta name="description" content=""/>
+  <meta name="keywords" content=""/> -->
+  
+  <title>Giddh Features</title>
+  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
+  <?php include 'head.php';?>
+
+</head>
+
+<!-- {oh:geo.country != 'IN'} -->
+<body id="page-top" class="light" ng-controller="homeController as vm">
+  
+  <!-- <div ng-class="{'other-country': geo.country != 'IN'}" ng-if="geo.country != 'IN'">
+    <iframe src="global.html" style="height:100%;width:100%;padding: 0;margin:0"></iframe>
+  </div> -->
+
+
+
+  <!-- <ng-include src="'header.html'"></ng-include> -->
+  <?php include 'header.php';?>  
+  <!-- end navigation -->
+
+  <section id="videoBg" class="pr black-back">
+    <!-- <div class="scroll-btn">
+      <a href="#feature" class="page-scroll"></a>
+    </div> -->
+   
+    <div class="intro home flex-box-banner slide show container">
+    <img src="../assets/images/Cloud Icon.svg" alt="" class="sky-one-01">
+        <div class="banner_intro features_banner">
+              <h1 class="banner_heading">Know your business' worth with simple online dashboard</h1>
+              <p class="banner_sub_heading m-t-20">Turn your accounting data into business intelligence with Giddh Dashboard.</p>
+              <p class="m-t-20">
+              <a class="btn btn-primary" id="downloadWindows" href="https://s3.ap-south-1.amazonaws.com/giddhbuildartifacts/giddh-app+Setup+6.4.0.exe">Download Free</a>
+                <a class="btn btn-primary" id="downloadMacOS" href="https://s3.ap-south-1.amazonaws.com/giddhbuildartifacts/giddh-app-6.4.0.dmg">Download Free</a>
+                <a class="btn btn-primary" id="downloadLinux" href="https://s3.ap-south-1.amazonaws.com/giddhbuildartifacts/giddh-app-6.4.0.tar.gz">Download Free</a>
+                <a class="talktosale" data-toggle="modal" data-target="#talkToSales_dialog"><span class="glyphicon glyphicon-earphone"></span>Talk to Sales</a>
+            </p>
+        </div>
+        <div class="right-section">
+          <!-- <img src="../assets/images/Cloud Icon.svg" alt="" class="sky-one bn-3"> -->
+          <img src="../assets/images/Cloud Icon.svg" alt="" class="sky-two bn-3">
+          <img src="../assets/images/dashboard/Banner.png" alt="Tally your books with giddh" class="tallybook">
+        </div>
+    </div>
+  </section> <!-- end of videoBg -->
+  
+  <!-- home content end -->
+  <section class="graybg graybg-reporting  homeContent" id="feature">
+      <div class="container">
+              <div class="row m-b-60">
+                    <div class="col-sm-12">
+                      <h2 class="center_heading">Assess your numbers in one glance</h2>
+                      <p>Gain actionable insights and easily evaluate growth of your business. Real-time accounting dashboard offers you a bird eye’s view to your business health on a single window. Actively keep an eye on the net worth, revenue, profit/loss and other key information of your business.</p>
+                      <a class="btn btn-primary btn-lg m-t-70" data-toggle="modal" data-target="#talkToSales_dialog">Talk to Sales</a>
+                  </div>
+              </div>
+             
+             </div>
+      
+  </section> 
+  <section class="whitebg homeContent left-text">
+  <div class="container">
+  <div class="row">
+  <div class="col-sm-6">
+  <h2>Know the true worth of your business</h2>
+  <p>There’s no way to know exactly where your business stands without knowing what your net worth is, whether your revenue is growing and where your expenses lie. Online accounting dashboard gives you the real position of your all these values by automatically crunching your numbers and presenting them on an easy to evaluate graph.</p>
+  </div>
+  
+  <div class="col-sm-6 text-center">
+   <img src="../assets/images/dashboard/Group-4870.png" alt="">
+  </div>
+  <hr>
+  
+  </div>
+  <hr>
+  
+  <div class="row">
+  <div class="col-sm-6 text-center">
+   <img src="../assets/images/dashboard/Group-4786.png" alt="">
+  </div>
+  <div class="col-sm-6">
+  <h2>Track your monthly overdues</h2>
+  <p>Pay and get paid quicker by easily following up on your monthly overdues. Dashboard gives a summary of what you owe to your clients and what’s owed to you in the form of receivable and payable right on your home window.</p>
+  </div>
+  
+  
+ 
+  
+  </div>
+   <hr>
+  
+
+  <div class="row">
+   
+  <div class="col-sm-6">
+  <h2 class="collab">Keep an eye on where your money goes</h2>
+  <p>Keep track of your expenses & discounts you give. Giddh accounting dashboard provides you with easy to understand expense graph so that you can create better strategies to minimise expenses & maximise profits.</p>
+  </div>
+  
+  <div class="col-sm-6 text-center">
+   <img src="../assets/images/dashboard/Group-4808.png" alt="">
+  </div>
+ </div>
+   <hr>
+
+ 
+ <div class="row">
+   <div class="col-sm-6 text-center">
+   <img src="../assets/images/dashboard/Group-4809.png" alt="">
+  </div>
+   
+  <div class="col-sm-6">
+  <h2 class="collab">Predict with more precision</h2>
+  <p>Giddh takes the pain out from your prediction process. With one-click yearly comparison of revenues, profit/loss & expenses, you can make better assumptions of how the business will behave in the future by the past trends.</p>
+  </div>
+  
+  
+ </div>
+ 
+ <div class="row">
+   
+   
+  <div class="col-sm-6">
+  <h2 class="collab">Financial ratio analysis</h2>
+  <p>Will your business be able to pay short-term and long-term obligations? What is its liquidity and profitability? The ratio analysis calculated automatically from the standard business formulas gives you an idea about your liability, equity and more so that you can take appropriate measures at the right time.</p>
+  </div>
+  
+  <div class="col-sm-6 text-center">
+   <img src="../assets/images/dashboard/Group-4832.png" alt="">
+  </div>
+  
+  
+ </div>
+ 
+   
+   
+              
+   
+  </div>
+  </section> 
+  
+  
+  
+  
+  
+  <section class="graybg p-t-70">
+                      <div class="container area-flex">
+                           <div class="pioneer col-md-offset-2"><img src="../assets/images/dashboard/Blog.png" alt=""></div>
+                            <span class="blog-reporting">Optimise, Analyse and Evaluate with <br>
+Giddh Dynamic Dashboard<br>
+
+                            <a href="#">Read more...</a>
+                            </span>
+                      </div>
+                  </section>
+  
+  <section class="whitebg know p-t-80">
+<div class="container">
+<div class="row p-tb-0">
+                <div class="col-sm-12">
+                    <div class="text-center">
+                    <img src="../assets/images/reporting/footer.svg" class="peopel-img" alt="">
+                       
+                       <div class="reporting-blog-text">
+                       <h2>People interested in invoicing also looked at</h2>
+                       <ul class="reporting-blog-menu">
+                       <li><a href="#">Invoicing</a></li>
+                       <li><a href="#">Multi-Currency</a></li>
+                       <li><a href="#">Security</a></li>
+                       <li><a href="#">All features</a></li>
+                       </ul>
+                       
+                       </div>
+                       
+                    </div>
+                </div>
+            </div>
+</div>
+
+</section>
+  
+  <!-- <div class="modal fade" id="talkToSales_dialog" role="dialog">
+  <div class="modal-dialog custom-dialog">
+    <div class="modal-header">
+      <h3 class="brdB left">Talk to Expert</h3>
+      <a data-dismiss="modal" aria-label="Close" class="close-glyph">✕</a>
+       <ul class="nav-tabs nav-tabs-talk height42" id="shortner_tabs">
+           <li class="active"><a href="#scheduleCall"  data-toggle="tab">Arrange a Demo</a></li>
+           <li><a href="#dropEmail"  data-toggle="tab">Drop us a mail</a></li>
+
+       </ul>
+    </div>
+       <div class="pd2 tab-content custom-padding-20 height-552 tab-content-modal">
+
+         <div id="dropEmail" class="tab-pane">
+           <div id="Jkvq" class="talktosale-superform"></div>
+
+           <div class='alert alert-succcess successsales'></div>
+         </div>
+
+         <div id="scheduleCall" class="tab-pane active vertical-center">
+                <p>Choose your preferred time to schedule<br>
+                   a demo with an expert. </p>
+                <button class="btn btn-primary">Click Here</button>
+         </div>
+       </div>
+       
+     </div>
+ </div> -->
+  <!-- end of page -->
+  <?php include 'footScript.php';?>
+  <?php include 'footer.php';?> 
+  <script>
+  function detectBrowser(x, y, z) {
+    z = z || y;
+    if (navigator[x] && navigator[x].indexOf(y) !== -1) {
+        osName = z;
+    }
+}
+
+/*   navigator     value     download  */
+detectBrowser( "appVersion", "Mac",    "MacOS"   );
+detectBrowser( "appVersion", "Linux"             );
+detectBrowser( "userAgent",  "Linux"             );
+detectBrowser( "platform",   "Linux"             );
+detectBrowser( "appVersion", "Win",    "Windows" );
+detectBrowser( "userAgent",  "Windows"           );
+detectBrowser( "platform",   "Win",    "Windows" );
+detectBrowser( "oscpu",      "Windows"           );
+document.getElementById("download"+osName).className = "btn btn-primary knownOS";
+
+  </script>
+</body>
+</html>
