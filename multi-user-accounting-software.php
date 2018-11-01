ce840c28fc8271ef55daeb95052760bb58821700
Arpit Meena <arpitmeena@walkover.in>
2018-11-01 16:16:35 +0530

    Arpit | new meta tags added by synram

---
+++ multi-user-accounting-software.php
@@ -0,0 +1,227 @@
+<!DOCTYPE HTML>
+<html lang="en" ng-app="giddhWebsite">
+  <meta charset="UTF-8">
+  <!-- 
+  <meta name="keywords" content=""/> -->
+  <meta name="description" content=" Giddh multi user accounting software helps you to sync all your department and work together. Share business data with an external team CA, Accountant etc. & key member in your team online.  "/>
+  
+  <title>Multi User Accounting Software </title>
+  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
+  <?php include 'head.php';?>
+
+</head>
+
+<!-- {oh:geo.country != 'IN'} -->
+<body id="page-top" class="light" ng-controller="homeController as vm">
+  
+  <!-- <div ng-class="{'other-country': geo.country != 'IN'}" ng-if="geo.country != 'IN'">
+    <iframe src="global.html" style="height:100%;width:100%;padding: 0;margin:0"></iframe>
+  </div> -->
+
+
+
+  <!-- <ng-include src="'header.html'"></ng-include> -->
+  <?php include 'header.php';?>  
+  <!-- end navigation -->
+
+  <section id="videoBg" class="black-back">
+    <!-- <div class="scroll-btn">
+      <a href="#feature" class="page-scroll"></a>
+    </div> -->
+   
+    <div class="intro home flex-box-banner slide show container">
+    <img src="../assets/images/Cloud Icon.svg" alt="" class="sky-one-01">
+        <div class="banner_intro features_banner">
+              <h1 class="banner_heading_new">Work better together by sharing business insights</h1>
+              <p class="banner_sub_heading m-t-20">Allow external accountants & internal team members <br>across different departments to work together <br>seamlessly.</p>
+              <p class="m-t-20">
+              <a class="btn btn-primary" id="downloadWindows" href="https://s3.ap-south-1.amazonaws.com/giddhbuildartifacts/giddh-app+Setup+6.4.0.exe">Download Free</a>
+                <a class="btn btn-primary" id="downloadMacOS" href="https://s3.ap-south-1.amazonaws.com/giddhbuildartifacts/giddh-app-6.4.0.dmg">Download Free</a>
+                <a class="btn btn-primary" id="downloadLinux" href="https://s3.ap-south-1.amazonaws.com/giddhbuildartifacts/giddh-app-6.4.0.tar.gz">Download Free</a>
+                <a class="talktosale" data-toggle="modal" data-target="#talkToSales_dialog"><span class="glyphicon glyphicon-earphone"></span>Talk to Sales</a>
+            </p>
+        </div>
+        <div class="right-section">
+          <!-- <img src="../assets/images/Cloud Icon.svg" alt="" class="sky-one bn-3"> -->
+          <img src="../assets/images/Cloud Icon.svg" alt="" class="sky-two bn-3">
+          <img src="../assets/images/share-data/Banner.png" alt="Tally your books with giddh" class="tallybook">
+        </div>
+    </div>
+  </section> <!-- end of videoBg -->
+  
+  <!-- home content end -->
+  <section class="graybg graybg-reporting  homeContent" id="feature">
+      <div class="container">
+              <div class="row m-b-60">
+                    <div class="col-sm-12">
+                      <h2 class="center_heading">Smart collaboration across all departments</h2>
+                      <p>Multi user accounting software enables business owners & accountants to collaborate and make the best of their numbers. With real time sharing of financial data, you can derive meaningful insight and run your business with efficiency.</p>
+                      <a class="btn btn-primary btn-lg m-t-70" data-toggle="modal" data-target="#talkToSales_dialog">Talk to Sales</a>
+                  </div>
+              </div>
+             
+             </div>
+      
+  </section> 
+  <section class="whitebg homeContent left-text">
+  <div class="container">
+  <div class="row">
+  <div class="col-sm-6">
+  <h2>Always be on the driver’s seat.</h2>
+  <p>Giddh multi-user online accounting software gives you the power to provide specified controlled access to different stakeholders who want access to a specific part of your financial data. With our unique role-based model sharing becomes easy, fast and super secure. You can </p>
+  </div>
+  
+  <div class="col-sm-6 text-center">
+   <img src="../assets/images/share-data/Group-4865.png" alt="">
+  </div>
+  <div class="col-sm-12">
+  <ul class="reportint-list">
+                  <li>Give permission to multiple users based on their email addresses.</li>
+                  <li>Control which user sees what part of your financial data and always be in control.</li>
+                  <li>Give IP based access to your accountants in case you want them to work on your books at only your office or premise.</li>
+                  <li>Revoke the given permission to your staff to view or make changes to your data once the desired action is performed.
+
+</li>
+<li>Create your own custom roles according to the need of your business and delete them later.</li>
+                  </ul>
+  </div>
+  
+  <hr>
+  
+  </div>
+  <hr>
+  
+  <div class="row">
+  <div class="col-sm-6 text-center">
+   <img src="../assets/images/share-data/Group-4883.png" alt="">
+  </div>
+  <div class="col-sm-6">
+  <h2>Collaborate across multiple departments</h2>
+  <p>Give your staff specific access to the different accounting modules they need to do their job. With multi-user accounting, your sales manager will be happy with real time invoice sharing and your accounting staff will always be on top of the tax filing.And when it matters the most, you can get your advisor on board to speed up the process of decision making.</p>
+  </div>
+  
+  
+ 
+  
+  </div>
+   <hr>
+  
+
+  <div class="row">
+   
+  <div class="col-sm-6">
+  <h2 class="collab">Avoid unnecessary confusion with ledger sharing</h2>
+  <p>Giddh multi-user accounting software lets you share your ledger with view access to external accounting team or client so that all the discrepancies can be avoided. Additionally, witness the magic of magic link in ledger sharing. Magic link comes with the validity of 24 hours, after which your shared ledger will not be accessible.</p>
+  </div>
+  
+  <div class="col-sm-6 text-center">
+   <img src="../assets/images/share-data/Group-4871.png" alt="">
+  </div>
+ </div>
+
+
+ 
+ 
+ 
+ 
+ 
+   
+   
+              
+   
+  </div>
+  </section> 
+  
+  
+  
+  
+  
+  <section class="graybg p-t-70">
+                      <div class="container area-flex">
+                           <div class="pioneer col-md-offset-2"><img src="../assets/images/share-data/Blog.png" alt=""></div>
+                            <span class="blog-reporting">Real time success with <br>
+real time accounting!<br>
+
+                            <a href="#">Read more...</a>
+                            </span>
+                      </div>
+                  </section>
+  
+  <section class="whitebg know p-t-80">
+<div class="container">
+<div class="row p-tb-0">
+                <div class="col-sm-12">
+                    <div class="text-center">
+                    <img src="../assets/images/reporting/footer.svg" class="peopel-img" alt="">
+                       
+                       <div class="reporting-blog-text">
+                       <h2>People interested in invoicing also looked at</h2>
+                       <ul class="reporting-blog-menu">
+                       <li><a href="#">Invoicing</a></li>
+                       <li><a href="#">Multi-Currency</a></li>
+                       <li><a href="#">Security</a></li>
+                       <li><a href="#">All features</a></li>
+                       </ul>
+                       
+                       </div>
+                       
+                    </div>
+                </div>
+            </div>
+</div>
+
+</section>
+  
+  <!-- <div class="modal fade" id="talkToSales_dialog" role="dialog">
+  <div class="modal-dialog custom-dialog">
+    <div class="modal-header">
+      <h3 class="brdB left">Talk to Expert</h3>
+      <a data-dismiss="modal" aria-label="Close" class="close-glyph">✕</a>
+       <ul class="nav-tabs nav-tabs-talk height42" id="shortner_tabs">
+           <li class="active"><a href="#scheduleCall"  data-toggle="tab">Arrange a Demo</a></li>
+           <li><a href="#dropEmail"  data-toggle="tab">Drop us a mail</a></li>
+
+       </ul>
+    </div>
+       <div class="pd2 tab-content custom-padding-20 height-552 tab-content-modal">
+
+         <div id="dropEmail" class="tab-pane">
+           <div id="Jkvq" class="talktosale-superform"></div>
+
+           <div class='alert alert-succcess successsales'></div>
+         </div>
+
+         <div id="scheduleCall" class="tab-pane active vertical-center">
+                <p>Choose your preferred time to schedule<br>
+                   a demo with an expert. </p>
+                <button class="btn btn-primary">Click Here</button>
+         </div>
+       </div>
+       
+     </div>
+ </div> -->
+  <!-- end of page -->
+  <?php include 'footScript.php';?>
+  <?php include 'footer.php';?> 
+  <script>
+  function detectBrowser(x, y, z) {
+    z = z || y;
+    if (navigator[x] && navigator[x].indexOf(y) !== -1) {
+        osName = z;
+    }
+}
+
+/*   navigator     value     download  */
+detectBrowser( "appVersion", "Mac",    "MacOS"   );
+detectBrowser( "appVersion", "Linux"             );
+detectBrowser( "userAgent",  "Linux"             );
+detectBrowser( "platform",   "Linux"             );
+detectBrowser( "appVersion", "Win",    "Windows" );
+detectBrowser( "userAgent",  "Windows"           );
+detectBrowser( "platform",   "Win",    "Windows" );
+detectBrowser( "oscpu",      "Windows"           );
+document.getElementById("download"+osName).className = "btn btn-primary knownOS";
+
+  </script>
+</body>
+</html>
