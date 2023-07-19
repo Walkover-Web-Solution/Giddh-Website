1776e895fb8970ed966b25134a3be69ca778070a
Divyanshu <divyanshushrivastava123@gmail.com>
2023-07-19 16:42:48 +0530

    Start working on mobile responsive

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,67 @@
 const navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-dark px-2 py-0 position-fixed w-100">
+      <nav className="navbar navbar-expand-lg py-0 position-fixed w-100">
         <div className="container-fluid">
+          <a className="navbar--navbar_brand" href="/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="126.833"
+              height="25"
+              viewBox="0 0 126.833 25"
+            >
+              <g
+                id="giddh_logo"
+                className="logo-white"
+                data-name="Group 1"
+                transform="translate(-319.076 -1919.767)"
+              >
+                <rect
+                  id="giddh_0"
+                  className="giddh-logo-svg"
+                  data-name="Giddh-logo"
+                  width="5.491"
+                  height="24.999"
+                  transform="translate(349.463 1919.767)"
+                  fill="#1a237e"
+                />
+                <path
+                  id="giddh_1"
+                  className="giddh-logo-svg"
+                  data-name="Giddh-logo"
+                  d="M1156.769,1954.394h-8.975c-7.426,0-14.009-4.343-14.009-12.429,0-9.145,6.723-12.57,14.748-12.57h8.236v11.683h-5.491v-6.6h-3.238c-4.646,0-8.553,2.295-8.553,7.2,0,5.685,3.837,7.627,8.94,7.627h8.342Z"
+                  transform="translate(-771.519 -9.628)"
+                  fill="#1a237e"
+                />
+                <path
+                  id="giddh_2"
+                  className="giddh-logo-svg"
+                  data-name="Giddh-logo"
+                  d="M2174.323,1954.4h5.49v-10.8h10.665v10.8h5.491v-25h-5.491v9.322h-10.665V1929.4h-5.49Z"
+                  transform="translate(-1750.06 -9.635)"
+                  fill="#1a237e"
+                />
+                <path
+                  id="giddh_3"
+                  className="giddh-logo-svg"
+                  data-name="Giddh-logo"
+                  d="M1698.978,1929.4h8.975c7.427,0,14.008,4.343,14.008,12.429,0,9.146-6.723,12.57-14.748,12.57h-8.236v-11.683h5.49v6.6h3.238c4.646,0,8.553-2.295,8.553-7.2,0-5.685-3.836-7.627-8.94-7.627h-8.342Z"
+                  transform="translate(-1304.837 -9.628)"
+                  fill="#1a237e"
+                />
+                <path
+                  id="giddh_4"
+                  className="giddh-logo-svg"
+                  data-name="Giddh-logo"
+                  d="M341.488,1929.686h-9.278v4.838h4.254v3.865a10.206,10.206,0,0,1-4.924,1.176c-4.455,0-7.368-3.225-7.368-7.661s2.914-7.661,7.368-7.661a7.283,7.283,0,0,1,5.761,2.251l3.684-4.032c-2.378-2.218-5.694-3.058-9.445-3.058-7.4,0-12.795,4.873-12.795,12.5s5.392,12.5,12.795,12.5a21.2,21.2,0,0,0,9.948-2.285Z"
+                  transform="translate(0.33 0.363)"
+                  fill="#1a237e"
+                />
+              </g>
+            </svg>
+          </a>
           <button
-            className="navbar-toggler text-light"
+            className="navbar-toggler ms-auto"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarTogglerDemo01"
@@ -12,66 +69,40 @@ const navbar = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
+            <svg
+              width="33"
+              height="20"
+              viewBox="0 0 33 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <line
+                className="hamberger_icon"
+                y1="18.5"
+                x2="33"
+                y2="18.5"
+                stroke="#444444"
+                stroke-width="3"
+              />
+              <line
+                className="hamberger_icon"
+                y1="9.5"
+                x2="33"
+                y2="9.5"
+                stroke="#444444"
+                stroke-width="3"
+              />
+              <line
+                className="hamberger_icon"
+                y1="1.5"
+                x2="33"
+                y2="1.5"
+                stroke="#444444"
+                stroke-width="3"
+              />
+            </svg>
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a className="navbar-brand" href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="126.833"
-                height="25"
-                viewBox="0 0 126.833 25"
-              >
-                <g
-                  id="giddh_logo"
-                  className="logo-white"
-                  data-name="Group 1"
-                  transform="translate(-319.076 -1919.767)"
-                >
-                  <rect
-                    id="giddh_0"
-                    className="giddh-logo-svg"
-                    data-name="Giddh-logo"
-                    width="5.491"
-                    height="24.999"
-                    transform="translate(349.463 1919.767)"
-                    fill="#1a237e"
-                  />
-                  <path
-                    id="giddh_1"
-                    className="giddh-logo-svg"
-                    data-name="Giddh-logo"
-                    d="M1156.769,1954.394h-8.975c-7.426,0-14.009-4.343-14.009-12.429,0-9.145,6.723-12.57,14.748-12.57h8.236v11.683h-5.491v-6.6h-3.238c-4.646,0-8.553,2.295-8.553,7.2,0,5.685,3.837,7.627,8.94,7.627h8.342Z"
-                    transform="translate(-771.519 -9.628)"
-                    fill="#1a237e"
-                  />
-                  <path
-                    id="giddh_2"
-                    className="giddh-logo-svg"
-                    data-name="Giddh-logo"
-                    d="M2174.323,1954.4h5.49v-10.8h10.665v10.8h5.491v-25h-5.491v9.322h-10.665V1929.4h-5.49Z"
-                    transform="translate(-1750.06 -9.635)"
-                    fill="#1a237e"
-                  />
-                  <path
-                    id="giddh_3"
-                    className="giddh-logo-svg"
-                    data-name="Giddh-logo"
-                    d="M1698.978,1929.4h8.975c7.427,0,14.008,4.343,14.008,12.429,0,9.146-6.723,12.57-14.748,12.57h-8.236v-11.683h5.49v6.6h3.238c4.646,0,8.553-2.295,8.553-7.2,0-5.685-3.836-7.627-8.94-7.627h-8.342Z"
-                    transform="translate(-1304.837 -9.628)"
-                    fill="#1a237e"
-                  />
-                  <path
-                    id="giddh_4"
-                    className="giddh-logo-svg"
-                    data-name="Giddh-logo"
-                    d="M341.488,1929.686h-9.278v4.838h4.254v3.865a10.206,10.206,0,0,1-4.924,1.176c-4.455,0-7.368-3.225-7.368-7.661s2.914-7.661,7.368-7.661a7.283,7.283,0,0,1,5.761,2.251l3.684-4.032c-2.378-2.218-5.694-3.058-9.445-3.058-7.4,0-12.795,4.873-12.795,12.5s5.392,12.5,12.795,12.5a21.2,21.2,0,0,0,9.948-2.285Z"
-                    transform="translate(0.33 0.363)"
-                    fill="#1a237e"
-                  />
-                </g>
-              </svg>
-            </a>
             <ul className="navbar-nav ms-auto text-light mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" href="#">
@@ -82,80 +113,74 @@ const navbar = () => {
                 <a className="nav-link" href="#">
                   Features
                 </a>
-                <ul className="dropdown-menu">
+                <ul className="dropdown-menu dropdown-menu--position-left">
                   <li>
                     <ul className="dropdown-menu__wapper d-flex flex-wrap ps-0">
-                      <li className="col-3">
+                      <li className="col-lg-3 col-md-4 col-sm-6">
                         <ul className="ps-0">
                           <li></li>
                           <li>
-                            <a href="#">Menu name 1</a>
+                            <a href="#">GST Compliances</a>
                           </li>
                           <li>
-                            <a href="#">Menu name 1</a>
+                            <a href="#">Bank Reconcile</a>
                           </li>
                           <li>
-                            <a href="#">Menu name 1</a>
+                            <a href="#">Multi Currency</a>
                           </li>
                           <li>
-                            <a href="#">Menu name 1</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li className="col-3">
-                        <ul className="ps-0">
-                          <li>
-                            <a href="#">Menu name 1</a>
+                            <a href="#">ICICI Integration</a>
                           </li>
                           <li>
-                            <a href="#">Menu name 1</a>
+                            <a href="#">API Integration</a>
                           </li>
                           <li>
-                            <a href="#">Menu name 1</a>
-                          </li>
-                          <li>
-                            <a href="#">Menu name 1</a>
-                          </li>
-                          <li>
-                            <a href="#">Menu name 1</a>
+                            <a href="#">E-Invoice</a>
                           </li>
                         </ul>
                       </li>
-                      <li className="col-3">
+                      <li className="col-lg-3 col-md-4 col-sm-6">
                         <ul className="ps-0">
                           <li>
-                            <a href="#">Menu name 1</a>
+                            <a href="#">Invoicing</a>
                           </li>
                           <li>
-                            <a href="#">Menu name 1</a>
+                            <a href="#">Reports & Analysis</a>
                           </li>
                           <li>
-                            <a href="#">Menu name 1</a>
+                            <a href="#">Share Data</a>
                           </li>
                           <li>
-                            <a href="#">Menu name 1</a>
-                          </li>
-                          <li>
-                            <a href="#">Menu name 1</a>
+                            <a href="#">Contacts</a>
                           </li>
                         </ul>
                       </li>
-                      <li className="col-3">
+                      <li className="col-lg-3 col-md-4 col-sm-6">
                         <ul className="ps-0">
                           <li>
-                            <a href="#">Menu name 1</a>
+                            <a href="#">Dashboard</a>
                           </li>
                           <li>
-                            <a href="#">Menu name 1</a>
+                            <a href="#">Import Excel Files</a>
                           </li>
                           <li>
-                            <a href="#">Menu name 1</a>
+                            <a href="#">Inventory</a>
                           </li>
                           <li>
-                            <a href="#">Menu name 1</a>
+                            <a href="#">Security</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li className="col-lg-3 col-md-4 col-sm-6">
+                        <ul className="ps-0">
+                          <li>
+                            <a href="#">Anywhere-Anytime</a>
                           </li>
                           <li>
-                            <a href="#">Menu name 1</a>
+                            <a href="#">Linked Bank Account</a>
+                          </li>
+                          <li>
+                            <a href="#">All Features</a>
                           </li>
                         </ul>
                       </li>
@@ -183,42 +208,19 @@ const navbar = () => {
                   Resources
                 </a>
                 <ul className="dropdown-menu">
-                  <li><a href="#">Blog</a></li>
-                  <li><a href="#">Guide</a></li>
-                  <li><a href="#">Help</a></li>
+                  <li>
+                    <a href="#">Blog</a>
+                  </li>
+                  <li>
+                    <a href="#">Guide</a>
+                  </li>
+                  <li>
+                    <a href="#">Help</a>
+                  </li>
                 </ul>
               </li>
             </ul>
-            <div className="navright d-md-flex align-items-center">
-              <div>
-                <div className="dropdown">
-                  <button
-                    className="btn btn-secondary dropdown-toggle"
-                    type="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Dropdown button
-                  </button>
-                  <ul className="dropdown-menu">
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+            <div className="nav_right d-md-flex align-items-center">
               <div>
                 <a href="#">
                   <svg
@@ -242,12 +244,12 @@ const navbar = () => {
                   </svg>
                 </a>
               </div>
-              <div className="ms-3">
+              <div>
                 <a href="#" className="login_page_link">
                   Login
                 </a>
               </div>
-              <div className="ms-1">
+              <div>
                 <a href="#" className="signup_page_link">
                   Sign Up
                 </a>
