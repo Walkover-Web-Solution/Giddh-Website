1cb83c8cb179b06b2886a87323f5c4543d3897c2
pawan ajmera <pajmera205@gmail.com>
2023-07-19 18:19:43 +0530

    footer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,13 +2,14 @@ import "@/scss/global.scss";
 import "bootstrap/dist/css/bootstrap.css";
 import Header from "@/components/header";
 import Navbar from "@/components/navbar";
-
+import Footer from "@/components/footer";
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
     <Navbar/>
       <Header />
       <Component {...pageProps} />
+      <Footer/>
     </>
   );
 }
