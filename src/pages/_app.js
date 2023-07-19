da2289c171a34789d6ac717e7dfd0ff02bd5d366
piyussssh <mvps1862@gmail.com>
2023-07-19 12:35:59 +0530

    bootstrap.min

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,12 @@ import "@/scss/global.scss";
 import "bootstrap/dist/css/bootstrap.css";
 import Header from "@/components/header";
 import Navbar from "@/components/navbar";
+import { useEffect } from "react";
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
   return (
     <>
     <Navbar/>
