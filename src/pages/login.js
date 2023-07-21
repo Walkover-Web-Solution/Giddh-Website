8b92f6f83494118338fca31e63573e59468553d9
Divyanshu <divyanshushrivastava123@gmail.com>
2023-07-21 20:11:27 +0530

    signup page 1st screen complete with responsive

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,20 +1,20 @@
 const logIn = () => {
   return (
     <>
-      <section className="login d-flex  ">
-        <div className="login__left_section col-lg-3 col-md-6">
+      <section className="entry d-flex  ">
+        <div className="entry__left_section col-xl-3 col-lg-4 col-md-5">
           <img
             src="/img/giddh-logo.svg"
-            className="login__left_section__brand_logo"
+            className="entry__left_section__brand_logo"
           />
           <img
             src="/img/login-page-left-section-banner.svg"
-            className="login__left_section__banner pe-4 img-fluid"
+            className="entry__left_section__banner pe-4 img-fluid"
           />
         </div>
-        <div className="login__right_section col-lg-9 col-md-6 col-sm-12 col-12">
-          <div className="login__right_section__container">
-            <div className="d-none mb-5 login__right_section__container--logo-visible-in-small">
+        <div className="entry__right_section col-xl-9 col-lg-8 col-md-7 col-sm-12 col-12">
+          <div className="entry__right_section__container">
+            <div className="d-none mb-5 entry__right_section__container--logo-visible-in-small">
               <img
                 src="/img/giddh-logo.svg"
                 width="auto"
@@ -24,11 +24,11 @@ const logIn = () => {
               />
             </div>
             <h1>Welcome back!</h1>
-            <div className="login__right_section__container__login_with mb-4">
+            <div className="entry__right_section__container__entry_with mb-4">
               <span className="d-inline-block mb-4">Login with</span>
 
               <div className="d-flex align-items-center">
-                <button className="login__right_section__container__login_with--btn-with-text">
+                <button className="entry__right_section__container__entry_with--btn-with-text">
                   <img
                     src="/img/google-logo.svg"
                     width="24px"
@@ -42,14 +42,14 @@ const logIn = () => {
 
             <span className="d-block line_on_right c-fs-6 mb-4">or</span>
 
-            <button className="login__right_section__container__login_button mb-4">
+            <button className="entry__right_section__container__entry_button mb-4">
               Login with OTP
             </button>
 
             <p className="c-fs-6 mb-4">
-              Trouble logging in ? <span className="text_blue">Click here</span>
+              Trouble logging in ? <a href="#" className="text_blue">Click here</a>
             </p>
-            <a className="c-fs-6 text_blue">Create new account</a>
+            <a href="#" className="c-fs-6 text_blue">Create new account</a>
           </div>
         </div>
       </section>
