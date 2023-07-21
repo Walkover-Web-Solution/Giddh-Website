6acf4bed2ba7ee4b079854d54999093dd4b7cef2
Ashish Kushwah <ashishkushawh888@gmail.com>
2023-07-21 12:49:11 +0530

    S1T009-C :: gst filing GSTR section 2 container completed A

--- src/pages/gst.js
+++ src/pages/gst.js
@@ -1,19 +1,20 @@
 const gst = () => {
   return (
     <>
-      <section>
-        <div className="container text-center h-100  ">
-          <div className="row d-flex">
+    
+      <section className="container mb-5 ">
+        <div className="gst-hero-container  text-center">
+          <div className="row d-flex h-100 align-content-end">
             
-              <div className="col">
-                <span className="c-fs-3 fw-light">
+              <div className=" d-flex flex-column  ">
+                <span className="c-fs-3  col-dark-light fw-light m-3">
                   From creating error-free GST invoice to
                 </span>
-                <h1 className="heading fw-bold  mb-6">
+                <h1 className="heading fw-bold  col-primary ">
                   File your GSTR Automatically
                 </h1>
               </div>
-              <div class="gst-image">
+              <div class="gst-image mt-auto">
                 <img
                   className="col-12"
                   alt="gst"
@@ -24,21 +25,21 @@ const gst = () => {
           </div>
         
       </section>
-      <section className="mt-4">
-        <div className="container mt-20">
-          <div className="text-center ">
-            <p className="gst-hading fw-bold ">
+      <section className=" filing-gstr py-5">
+        <div className="container mt-5">
+          <div className="text-center">
+            <p className="sub-heading c-fs-1 col-primary fw-bold ">
               Advantages of filing GSTR with us
             </p>
           </div>
 
           <div className="row">
             <div className="col">
-              <div className="filing-gstr d-flex flex-column align-items-center gap-4 p-3">
+              <div className="filing-gstr-card d-flex flex-column align-items-center gap-4 p-3">
                 <img src="/img/gst-images/Direct-Filing.svg" />
                 <div className="caption text-center">
-                  <h3>Direct Filing</h3>
-                  <p className="px-4 c-fs-3">
+                  <h3 className="small-heading col-primary fw-bold" >Direct Filing</h3>
+                  <p className="px-4 c-fs-5 lh-lg col-grey">
                     We prepare your GST return file automatically and do the
                     heavy lifting for you. Now file your Goods and Services Tax
                     (GST) directly on portal.
@@ -49,8 +50,8 @@ const gst = () => {
             <div className="col"> <div className="filing-gstr d-flex flex-column align-items-center gap-4 p-3">
                 <img src="/img/gst-images/accurate.svg" />
                 <div className="caption text-center">
-                  <h3>100% Accurate</h3>
-                  <p className="px-4 c-fs-3">
+                  <h3 className="small-heading col-primary fw-bold" >100% Accurate</h3>
+                  <p className="px-4 c-fs-5 lh-lg col-grey">
                   Get 10/10 on accuracy by identifying errors right before submitting your GST return online with the help of our straightforward error sheet.
                   </p>
                 </div>
@@ -58,8 +59,8 @@ const gst = () => {
             <div className="col"> <div className="filing-gstr d-flex flex-column align-items-center gap-4 p-3">
                 <img src="/img/gst-images/get-notified.svg" />
                 <div className="caption text-center">
-                  <h3>Timely Notifications</h3>
-                  <p className="px-4 c-fs-3">
+                  <h3 className="small-heading col-primary fw-bold">Timely Notifications</h3>
+                  <p className="px-4 c-fs-5 lh-lg col-grey">
                   Frustrated with last-minute tax return stress? With Giddh GST accounting, you will never miss another deadline or suffer the penalty.
                   </p>
                 </div>
@@ -70,8 +71,8 @@ const gst = () => {
             <div className="col"> <div className="filing-gstr d-flex flex-column align-items-center gap-4 p-3">
                 <img src="/img/gst-images/easy-fast.svg" />
                 <div className="caption text-center">
-                  <h3>Highly Collaborative</h3>
-                  <p className="px-4 c-fs-3">
+                  <h3 className="small-heading col-primary fw-bold">Highly Collaborative</h3>
+                  <p className="px-4 c-fs-5 lh-lg col-grey">
                   Be on the same page with your team. With our role based model, you can grant your CA the access to view the relevant GST tax return data.
                   </p>
                 </div>
@@ -79,8 +80,8 @@ const gst = () => {
             <div className="col"> <div className="filing-gstr d-flex flex-column align-items-center gap-4 p-3">
                 <img src="/img/gst-images/secure-reliable.svg" />
                 <div className="caption text-center">
-                  <h3>Secure & Reliable</h3>
-                  <p className="px-4 c-fs-3">
+                  <h3 className="small-heading col-primary fw-bold">Secure & Reliable</h3>
+                  <p className="px-4 c-fs-5 lh-lg col-grey">
                   With 2-factor authentication, industry leading security and OTP verification, your GST return filing process is secure from the beginning till the end.
                   </p>
                 </div>
@@ -88,8 +89,8 @@ const gst = () => {
             <div className="col"> <div className="filing-gstr d-flex flex-column align-items-center gap-4 p-3">
                 <img src="/img/gst-images/best-support.svg" />
                 <div className="caption text-center">
-                  <h3>Best in Class Support</h3>
-                  <p className="px-4 c-fs-3">
+                  <h3 className="small-heading col-primary fw-bold">Best in Class Support</h3>
+                  <p className="px-4 c-fs-5 lh-lg col-grey">
                   Stuck somewhere? Count on our friendly and knowledgable support team for your accounting & GST filing queries.
                   </p>
                 </div>
