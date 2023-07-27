f2cd55c0ae402f4f3d41afca9865d99b41f2a2b8
Divyanshu <divyanshushrivastava123@gmail.com>
2023-07-27 11:58:34 +0530

    start working on pricing

---
+++ src/pages/pricing.js
@@ -0,0 +1,13 @@
+const pricing = () => {
+    return (
+      <>
+        <section className="container-fluid pricing">
+          <div className="container">
+            
+          </div>
+        </section>
+      </>
+    );
+  };
+  export default pricing;
+  
