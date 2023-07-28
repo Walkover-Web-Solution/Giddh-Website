34201c277f68ddfa09f9402537dd20a9d6e3f748
Ashish Kushwah <ashishkushawh888@gmail.com>
2023-07-28 15:23:08 +0530

    S1T016  :  created the affiliate.js file

---
+++ src/pages/affiliate.js
@@ -0,0 +1,20 @@
+const affiliate = () => {
+  return (
+    <>
+      <section>
+        <div className="conatiner">
+          <div>
+            <h1>Why Join Us?</h1>
+            <div className="row col-md-10">
+                <div className="row">
+                    <div  className="col-2"><img src="/img/affilate/earn" /></div>
+                    <div className="col-10"></div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+export default affiliate;
