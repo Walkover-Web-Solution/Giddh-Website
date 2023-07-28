f07b80cee250210a932f5afbce361540246437d5
Ashish Kushwah <ashishkushawh888@gmail.com>
2023-07-28 14:52:34 +0530

    S1T002  : wroked in earth icon dropdwan

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,6 @@
 const Footer = () => {
-    return (
-
-        <div class="footer-container py-5" >
+  return (
+    <div class="footer-container py-5">
       <footer>
         <div className="top_footer">
           <div className="container">
@@ -11,20 +10,22 @@ const Footer = () => {
                   <div className="d-flex flex-column gap-3 .text-dark">
                     <h5>Quick Links</h5>
                     <ul className="footer-list list-unstyled mb-3">
-                      <li >
+                      <li>
                         <a href="">Pricing</a>
                       </li>
-                      <li >
+                      <li>
                         <a href="">About</a>
                       </li>
-                      <li >
+                      <li>
                         <a href="">Blog</a>
                       </li>
-                      <li >
+                      <li>
                         <a href="">Privacy Policy</a>
                       </li>
-                      <li >
-                        <a target="_blank" rel="noopener" href="">Work With Us</a>
+                      <li>
+                        <a target="_blank" rel="noopener" href="">
+                          Work With Us
+                        </a>
                       </li>
                       <li>
                         <a href="">Affiliate With Us</a>
@@ -36,108 +37,95 @@ const Footer = () => {
                   </div>
                 </div>
               </div>
-  
+
               <div className="col-sm-12 col-md-3">
                 <div className="d-flex flex-column gap-3">
                   <h5>Download</h5>
                   <div>
                     <p className="">Mobile Apps</p>
                     <ul className="list-unstyled">
-                      <li >
+                      <li>
                         <a className="download-icon" href="">
-                          <img
-                          
-                            alt=""
-                            src="/img/androide_icon.svg"
-                          />
-                          
+                          <img alt="" src="/img/androide_icon.svg" />
                         </a>
                       </li>
                     </ul>
                   </div>
-  
+
                   <div>
                     <p className="d-flex flex-column gap-3">Desktop Apps</p>
                     <ul className="list-unstyled d-flex gap-3">
-                      <li  className="">
+                      <li className="">
                         <a className="download-icon " href="">
-                        <img class =""
-                    src="/img/window-icon.svg"
-                          />
-                          
+                          <img class="" src="/img/window-icon.svg" />
                         </a>
-                      </li >
-                      <li >
+                      </li>
+                      <li>
                         <a className="download-icon rounded-circle" href="">
-                          <img 
-                            src="/img/mac_icon.svg"
-                          />
-                          
+                          <img src="/img/mac_icon.svg" />
                         </a>
                       </li>
                     </ul>
                   </div>
                 </div>
               </div>
-  
+
               <div className=" -sm-12 col-md-3 ">
-                <h5 className="list-unstyled d-flex flex-column gap-2">Contact Us</h5>
+                <h5 className="list-unstyled d-flex flex-column gap-2">
+                  Contact Us
+                </h5>
                 <div>
-                  <div className=" footer-dpn mb-3">
-                    <div className=" btn-group dropup ">
-                      <button
-                        type="button"
-                        className="btn btn-down dropdown-toggle contry-icon"
-                        data-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                      >
-                        <img
-                          src="img/earth.svg"
-                          alt=""
-                        />
-                        {" "}
+                  <div class="dropdown">
+                    <button
+                      className="btn earth-button  rounded-pill dropdown-toggle"
+                      type="button" 
+                      id="dropdownMenuButton"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                     <span><img src="img/earth.svg" alt=""/></span>
+                     IN - India
+                    </button>
+                    <div
+                      className="dropdown-menu"
+                      aria-labelledby="dropdownMenuButton"
+                    >
+                      <a className="dropdown-item" href="#">
                         IN - India
-                      </button>
-                      <div className="dropdown-menu">
-                        <a href="" className="dropdown-item">
-                          IN - India
-                        </a>
-                        <a href="" className="dropdown-item">
-                          UAE - United Arab Emirates
-                        </a>
-                        <a href="" className="dropdown-item">
-                          UK - United Kingdom
-                        </a>
-                      </div>
+                      </a>
+                      <a className="dropdown-item" href="#">
+                        UAE - United Arab Emirates
+                      </a>
+                      <a className="dropdown-item" href="#">
+                        {" "}
+                        UK - United Kingdom
+                      </a>
                     </div>
                   </div>
+
                   <ul className="footer-list list-unstyled class">
-                    <li
-                      
-                      className="widget_text widget widget_custom_html"
-                    >
-                     
+                    <li className="widget_text widget widget_custom_html">
                       <div className="textwidget custom-html-widget">
                         <ul className="footer-list list-unstyled class">
                           <li>
-                            Sales :{" "}
+                            Sales :
                             <a
                               href="/cdn-cgi/l/email-protection"
                               className="cf_email"
                               data-cfemail="4e3d2f222b3d0e29272a2a26602d2123"
                             >
-                              [email&#160;protected]
+                              sales@giddh.com
                             </a>
                           </li>
                           <li>
-                            Support:{" "}
+                            Support:
                             <a
                               href="/cdn-cgi/l/email-protection"
                               className="cf_email"
                               data-cfemail="fd8e888d8d928f89bd9a94999995d39e9290"
                             >
-                              [email&#160;protected]
+                              sales@giddh.com
                             </a>
                           </li>
                         </ul>
@@ -149,33 +137,34 @@ const Footer = () => {
                   </ul>
                 </div>
               </div>
-  
+
               <div className="col-sm-12 col-md-3">
                 <div className="location">
                   <h5>Locate Us</h5>
                   <div className="location-detail">
-                    <h4 >Indore</h4>
+                    <h4>Indore</h4>
                     <p>
-                      Walkover Web Solutions Pvt Ltd.<br /> 405-406, Capt. C.S.
-                      Naidu Arcade,<br /> Near Greater Kailash Hospital, 10/2 Old
-                      Palasia,<br /> Indore, Madhya Pradesh, India-452018
+                      Walkover Web Solutions Pvt Ltd.
+                      <br /> 405-406, Capt. C.S. Naidu Arcade,
+                      <br /> Near Greater Kailash Hospital, 10/2 Old Palasia,
+                      <br /> Indore, Madhya Pradesh, India-452018
                     </p>
                   </div>
                   <div className="location-detail">
                     <h4>USA</h4>
                     <p>
-                      USA Superheroes, Inc<br /> Sunset Lake Road, Suite B-2<br />{" "}
-                      DE - Delaware<br /> United States of America
+                      USA Superheroes, Inc
+                      <br /> Sunset Lake Road, Suite B-2
+                      <br />
+                      DE - Delaware
+                      <br /> United States of America
                     </p>
                   </div>
                 </div>
               </div>
             </div>
             <div className="d-flex mt-4 copyright-text">
-              <li
-              
-                className="widget_text widget widget_custom_html list-unstyled class"
-              >
+              <li className="widget_text widget widget_custom_html list-unstyled class">
                 <div className="textwidget custom-html-widget ">
                   All rights are reserved 2020 Walkover
                 </div>
@@ -184,9 +173,8 @@ const Footer = () => {
           </div>
         </div>
       </footer>
-      </div>
-    );
-  };
-  
-  export default Footer;
-  
+    </div>
+  );
+};
+
+export default Footer;
