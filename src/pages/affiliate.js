7b7bb27244dca9dd456db5d5b366d94bd5749abf
Ashish Kushwah <ashishkushawh888@gmail.com>
2023-07-28 18:20:06 +0530

    S1T016  :working on affiliate

--- src/pages/affiliate.js
+++ src/pages/affiliate.js
@@ -1,15 +1,85 @@
 const affiliate = () => {
   return (
     <>
-      <section>
-        <div className="conatiner">
-          <div>
-            <h1>Why Join Us?</h1>
-            <div className="row col-md-10">
-                <div className="row">
-                    <div  className="col-2"><img src="/img/affilate/earn" /></div>
-                    <div className="col-10"></div>
-                </div>
+      <section className="pt-5">
+        <div className="col-10 container">
+          <h1 className="text-center col-primary">Why Join Us?</h1>
+          <div className=" gap-4 d-flex container">
+            <div className="d-flex align-items-center flex-column flex-md-row affiliate flex-warp">
+              <div className="col-3 affiliate-img text-center">
+                <img src="/img/affilate/earn.png" />
+              </div>
+              <div className="col-9">
+                <h3 className="col-primary mb-2">
+                  Lucrative, Residual Commissions
+                </h3>
+                <p>
+                  Earn 50% of every new referral's first payment, 30% on renewal
+                  or second payment and 20% on the third one. We don't put a cap
+                  on earnings.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className=" gap-4 d-flex container">
+            <div className="d-flex align-items-center flex-column flex-md-row affiliate flex-warp">
+              <div className="col-3 affiliate-img text-center">
+                <img src="/img/affilate/target.png" />
+              </div>
+              <div className="col-9">
+                <h3 className="col-primary mb-2">
+                  Long Cookie Duration with Active Retargeting
+                </h3>
+                <p>
+                  Receive the credit you deserve with our 120-day cookie
+                  duration, plus benefit from our active retargeting campaigns.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className=" gap-4 d-flex container">
+            <div className="d-flex align-items-center flex-column flex-md-row affiliate flex-warp">
+              <div className="col-3 affiliate-img text-center">
+                <img src="/img/affilate/analytics.png" />
+              </div>
+              <div className="col-9">
+                <h3 className="col-primary mb-2">Reliable Tracking</h3>
+                <p>
+                  Accumulate more referrals with the highly-accurate tracking
+                  software we've implemented.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className=" gap-4 d-flex container">
+            <div className="d-flex align-items-center flex-column flex-md-row affiliate flex-warp">
+              <div className="col-3 affiliate-img text-center">
+                <img src="/img/affilate/meter.png" />
+              </div>
+              <div className="col-9">
+                <h3 className="col-primary mb-2">Proven Results</h3>
+                <p>
+                  Profit more from your efforts with our high conversion rate,
+                  strong earnings per click, and great customer lifetime value.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className=" gap-4 d-flex container">
+            <div className="d-flex align-items-center flex-column flex-md-row affiliate flex-warp">
+              <div className="col-3 affiliate-img text-center">
+                <img src="/img/affilate/free.png" />
+              </div>
+              <div className="col-9">
+                <h3 className="col-primary mb-2">Free To Join</h3>
+                <p>
+                It's free to join our partner community and only takes minutes to sign up and start promoting.
+                </p>
+              </div>
             </div>
           </div>
         </div>
