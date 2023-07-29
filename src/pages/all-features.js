f19200d6bef2ce2abe44b5877af351fd635cfbcf
Divyanshu <divyanshushrivastava123@gmail.com>
2023-07-29 15:29:36 +0530

    icici page  done

---
+++ src/pages/all-features.js
@@ -0,0 +1,289 @@
+import { MdClose } from "react-icons/md";
+const allFeatures = () => {
+  return (
+    <>
+      <div className="features">
+        <section className="container-fluid">
+          <div className="container features__heading_container">
+            <div className="row">
+              <div className="col-lg-7 order-lg-0 order-1">
+                <h2 className="heading col-primary fw-bold mb-4">
+                  Access your books anytime anywhere
+                </h2>
+                <p className="col-grey c-fs-3 mb-5">
+                  Manage your business finances anytime, from any
+                  internet-connected device – phone, laptop or computer.
+                </p>
+                <div className="features__heading_container__links">
+                  <a href="#" className="btn download_free_btn me-4">
+                    Download Free
+                  </a>
+                  <a href="#" className="col-blue">
+                    Schedule Demo
+                  </a>
+                </div>
+              </div>
+              <div className="col-lg-5 order-lg-1 order-0 position-relative">
+                <figure className="text-end">
+                  <img src="img/features-banner.svg" width="90%" />
+                  <img
+                    src="img/all-features-cloud_icon.svg"
+                    className="features__heading_container__right_banner_img"
+                    width="90%"
+                  />
+                </figure>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <section className="container-fluid feature-gallery all_features">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <div className="feature-gallery__card" data-bs-target="#featureModal" data-bs-toggle="modal">
+                <figure>
+                  <img
+                    src="/img/feature-inventory.svg"
+                    alt="Inventory Icon"
+                    height="90px"
+                    widht="auto"
+                  />
+                  <figcaption>Inventory</figcaption>
+                  <p>Create inventory easily and track inventory expenses. Managing inventory is no more a hassle!</p>
+                </figure>
+              </div>
+            </div>
+            <div className="col">
+              <div className="feature-gallery__card" data-bs-target="#featureModal" data-bs-toggle="modal">
+                <figure>
+                  <img
+                    src="/img/feature-gallery-icons/invoice.png"
+                    alt="Invoices Icon"
+                    height="90px"
+                    widht="auto"
+                  />
+                  <figcaption>Invoices</figcaption>
+                </figure>
+              </div>
+            </div>
+            <div className="col">
+              <div className="feature-gallery__card" data-bs-target="#featureModal" data-bs-toggle="modal">
+                <figure>
+                  <img
+                    src="/img/feature-gallery-icons/anywhere-anytime.png"
+                    alt="Anywhere Anytime Icon"
+                    height="90px"
+                    widht="auto"
+                  />
+                  <figcaption>Anywhere Anytime</figcaption>
+                </figure>
+              </div>
+            </div>
+            <div className="col">
+              <div className="feature-gallery__card" data-bs-target="#featureModal" data-bs-toggle="modal">
+                <figure>
+                  <img
+                    src="/img/feature-gallery-icons/bank-reconciliation.png"
+                    alt="Bank Reconciliation Icon"
+                    height="90px"
+                    widht="auto"
+                  />
+                  <figcaption>Bank Reconciliation</figcaption>
+                </figure>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="feature-gallery__card" data-bs-target="#featureModal" data-bs-toggle="modal">
+                <figure>
+                  <img
+                    src="/img/feature-gallery-icons/multicurrency.png"
+                    alt="Multi Currency Icon"
+                    height="90px"
+                    widht="auto"
+                  />
+                  <figcaption>Multi Currency</figcaption>
+                </figure>
+              </div>
+            </div>
+            <div className="col">
+              <div className="feature-gallery__card" data-bs-target="#featureModal" data-bs-toggle="modal">
+                <figure>
+                  <img
+                    src="/img/feature-gallery-icons/import-data.png"
+                    alt="Import Data Icon"
+                    height="90px"
+                    widht="auto"
+                  />
+                  <figcaption>Import Data</figcaption>
+                </figure>
+              </div>
+            </div>
+            <div className="col">
+              <div className="feature-gallery__card" data-bs-target="#featureModal" data-bs-toggle="modal">
+                <figure>
+                  <img
+                    src="/img/feature-gallery-icons/gst-compiance.png"
+                    alt="GST Compliances Icon"
+                    height="90px"
+                    widht="auto"
+                  />
+                  <figcaption>GST Compliances</figcaption>
+                </figure>
+              </div>
+            </div>
+            <div className="col">
+              <div className="feature-gallery__card" data-bs-target="#featureModal" data-bs-toggle="modal">
+                <figure>
+                  <img
+                    src="/img/feature-gallery-icons/report-analysis.png"
+                    alt="Reports & Analysis Icon"
+                    height="90px"
+                    widht="auto"
+                  />
+                  <figcaption>Reports & Analysis</figcaption>
+                </figure>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="feature-gallery__card" data-bs-target="#featureModal" data-bs-toggle="modal">
+                <figure>
+                  <img
+                    src="/img/feature-gallery-icons/share-data.png"
+                    alt="Share Data Icon"
+                    height="90px"
+                    widht="auto"
+                  />
+                  <figcaption>Share Data</figcaption>
+                </figure>
+              </div>
+            </div>
+            <div className="col">
+              <div className="feature-gallery__card" data-bs-target="#featureModal" data-bs-toggle="modal">
+                <figure>
+                  <img
+                    src="/img/feature-gallery-icons/dashboard-icon.png"
+                    alt="Dashboard Icon"
+                    height="90px"
+                    widht="auto"
+                  />
+                  <figcaption>Dashboard</figcaption>
+                </figure>
+              </div>
+            </div>
+            <div className="col">
+              <div className="feature-gallery__card" data-bs-target="#featureModal" data-bs-toggle="modal">
+                <figure>
+                  <img
+                    src="/img/feature-gallery-icons/support.png"
+                    alt="Audit Trail Icon"
+                    height="90px"
+                    widht="auto"
+                  />
+                  <figcaption>Audit Trail</figcaption>
+                </figure>
+              </div>
+            </div>
+            <div className="col">
+              <div className="feature-gallery__card" data-bs-target="#featureModal" data-bs-toggle="modal">
+                <figure>
+                  <img
+                    src="/img/feature-gallery-icons/support.png"
+                    alt="Audit Trail Icon"
+                    height="90px"
+                    widht="auto"
+                  />
+                  <figcaption>Audit Trail</figcaption>
+                </figure>
+              </div>
+            </div> 
+            <div className="col">
+              <div className="feature-gallery__card" data-bs-target="#featureModal" data-bs-toggle="modal">
+                <figure>
+                  <img
+                    src="/img/feature-gallery-icons/support.png"
+                    alt="Audit Trail Icon"
+                    height="90px"
+                    widht="auto"
+                  />
+                  <figcaption>Audit Trail</figcaption>
+                </figure>
+              </div>
+            </div> 
+            <div className="col">
+              <div className="feature-gallery__card" data-bs-target="#featureModal" data-bs-toggle="modal">
+                <figure>
+                  <img
+                    src="/img/feature-gallery-icons/support.png"
+                    alt="Audit Trail Icon"
+                    height="90px"
+                    widht="auto"
+                  />
+                  <figcaption>Audit Trail</figcaption>
+                </figure>
+              </div>
+            </div> 
+            <div className="col">
+              <div className="feature-gallery__card" data-bs-target="#featureModal" data-bs-toggle="modal">
+                <figure>
+                  <img
+                    src="/img/feature-gallery-icons/support.png"
+                    alt="Audit Trail Icon"
+                    height="90px"
+                    widht="auto"
+                  />
+                  <figcaption>Audit Trail</figcaption>
+                </figure>
+              </div>
+            </div>   
+            <div className="col">
+              <div className="feature-gallery__card" data-bs-target="#featureModal" data-bs-toggle="modal">
+                <figure>
+                  <img
+                    src="/img/feature-gallery-icons/support.png"
+                    alt="Audit Trail Icon"
+                    height="90px"
+                    widht="auto"
+                  />
+                  <figcaption>Audit Trail</figcaption>
+                </figure>
+              </div>
+            </div> 
+          </div>
+        </div>
+      </section>
+      <div
+        className="modal fade"
+        id="featureModal"
+        aria-hidden="true"
+        aria-labelledby="exampleModalToggleLabel"
+        tabIndex="-1"
+      >
+        <div className="modal-dialog modal-dialog-centered feature_gallery_modal">
+          <div className="modal-content">
+            <div className="modal-header position-relative">           
+              <button
+                type="button"
+                className="btn col-blue ms-auto p-0"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              >
+                <MdClose />
+              </button>
+            </div>
+            <div className="modal-body">
+              <h4 className="c-fs-1 c-fw-600 col-blue">INVOICES</h4>
+              <h5 className="c-fs-2 c-fw-600 col-primary mb-4">Get paid faster by creating professional looking invoices and send them automatically.</h5>
+              <p className="col-grey-deep mb-1">Create VAT compliant invoices and documents whether you are buying stocks for your inventory or selling your product or services to customers. Now create custom invoices and transact in multiple currencies. You don’t have to bother about the nuances of taxation as once set, the numbers are taken care of by Giddh.</p>
+              <a href="#" className="col-blue">More details</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default allFeatures;
