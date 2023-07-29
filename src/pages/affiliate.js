031f3b9a816b70e91bc7a01d34634dc9baf85dba
Ashish Kushwah <ashishkushawh888@gmail.com>
2023-07-29 13:39:41 +0530

    S1T016  : affiliate small card section

--- src/pages/affiliate.js
+++ src/pages/affiliate.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 const affiliate = () => {
   return (
     <>
@@ -8,18 +7,18 @@ const affiliate = () => {
           <h1 className="text-center sub-heading col-primary">Why Join Us?</h1>
           <div className="d-flex flex-column justify-content-center col-12">
             <section className="container col-12 col-lg-10 col-md-12 col-sm-12 d-grid gap-3 gap-sm-0">
-              <div className=" d-flex flex-column flex-md-row gap-2 align-items-center my-3">
+              <div className="d-flex flex-column flex-md-row gap-2 align-items-center my-3">
                 <div className="col-3 text-center affiliate align-content-center">
                   <img
                     className="mx-auto affiliate-img"
                     src="/img/affilate/earn.png"
                   />
                 </div>
-                <div className="col-9 col-md-8 ">
-                  <h4 className="c-fs-1  col-primary c-fw-400 mb-3">
+                <div className="col-9 col-md-8">
+                  <h4 className="c-fs-1 col-primary c-fw-400 mb-3">
                     Lucrative, Residual Commissions
                   </h4>
-                  <p p className="c-fs-5">
+                  <p className="c-fs-5">
                     Earn 50% of every new referral's first payment, 30% on
                     renewal or second payment and 20% on the third one. We don't
                     put a cap on earnings.
@@ -27,7 +26,6 @@ const affiliate = () => {
                 </div>
               </div>
             </section>
-
             <section className="container col-12 col-lg-10 col-md-12 col-sm-12 d-grid gap-3 gap-sm-0">
               <div className=" d-flex flex-column flex-md-row gap-2 align-items-center my-3">
                 <div className="col-3 text-center align-content-center">
@@ -40,7 +38,7 @@ const affiliate = () => {
                   <h4 className="c-fs-1  col-primary c-fw-400 mb-3">
                     Long Cookie Duration with Active Retargeting
                   </h4>
-                  <p p className="c-fs-5 ">
+                  <p className="c-fs-5">
                     Receive the credit you deserve with our 120-day cookie
                     duration, plus benefit from our active retargeting
                     campaigns.
@@ -48,7 +46,6 @@ const affiliate = () => {
                 </div>
               </div>
             </section>
-
             <section className="container col-12 col-lg-10 col-md-12 col-sm-12 d-grid gap-3 gap-sm-0">
               <div className=" d-flex flex-column flex-md-row gap-2 align-items-center ">
                 <div className="col-3 text-center align-content-center">
@@ -61,7 +58,7 @@ const affiliate = () => {
                   <h4 className="c-fs-1  col-primary c-fw-400 mb-3">
                     Reliable Tracking
                   </h4>
-                  <p p className="c-fs-5 ">
+                  <p className="c-fs-5">
                     Accumulate more referrals with the highly-accurate tracking
                     software we've implemented.
                   </p>
@@ -78,10 +75,10 @@ const affiliate = () => {
                   />
                 </div>
                 <div className="col-9 col-md-8">
-                  <h4 className="c-fs-1  col-primary c-fw-400 mb-3">
+                  <h4 className="c-fs-1 col-primary c-fw-400 mb-3">
                     Proven Results
                   </h4>
-                  <p p className="c-fs-5 ">
+                  <p className="c-fs-5">
                     Profit more from your efforts with our high conversion rate,
                     strong earnings per click, and great customer lifetime
                     value..
@@ -99,17 +96,17 @@ const affiliate = () => {
                   />
                 </div>
                 <div className="col-9 col-md-8">
-                  <h4 className="c-fs-1  col-primary c-fw-400 mb-3">
+                  <h4 className="c-fs-1 col-primary c-fw-400 mb-3">
                     Free To Join
                   </h4>
-                  <p p className="c-fs-5 ">
+                  <p className="c-fs-5">
                     It's free to join our partner community and only takes
                     minutes to sign up and start promoting.
                   </p>
                 </div>
               </div>
             </section>
-            <div className="text-center ">
+            <div className="text-center">
               <a href="" className="btn btn-start btn-lg">
                 <span className="col-white">Let's Get Started</span>
               </a>
@@ -123,15 +120,15 @@ const affiliate = () => {
           <div className="text-center col-primary pt-5">
             <h1>We Help you With Resources</h1>
           </div>
-          <section className="container  d-grid gap-3 my-5">
+          <section className="container d-grid gap-3 my-5">
             <div className=" d-flex flex-column flex-md-row gap-2 align-items-center my-3">
-              <img className="mx-auto Resources-img" src="/img/affilate/graph.png" />
+              <img className="mx-auto Resources-img" src="/img/affilate/graph.png"/>
               <div className="col-12 col-md-6">
                 <h4 className="c-fs-2 col-primary c-fw-600">
                   Dashboard To Track Your Success
                 </h4>
-                <p className="c-fs-4 ">
-                  Metrics are vital. As an Accounting Softwareaffiliate you will
+                <p className="c-fs-4">
+                  Metrics are vital. As an <a href="https://giddh.com/" title="Accounting Software" className="col-primary ">Accounting Software</a> affiliate you will
                   have your own reporting dashboard where you can track shares,
                   clicks, enrollments, and more. Keep track of your success and
                   know which types of promotions are working the best for your
@@ -140,8 +137,8 @@ const affiliate = () => {
               </div>
             </div>
           </section>
-          <section className="container  d-grid gap-3 my-5">
-            <div className=" d-flex flex-column flex-md-row-reverse gap-2 align-items-center my-3">
+          <section className="container d-grid gap-3 my-5">
+            <div className="d-flex flex-column flex-md-row-reverse gap-2 align-items-center my-3">
               <img
                 className="mx-auto Resources-img"
                 src="/img/affilate/content_creative.png"
@@ -150,7 +147,7 @@ const affiliate = () => {
                 <h4 className="c-fs-2 col-primary c-fw-600">
                   Creatives and Content
                 </h4>
-                <p p className="c-fs-4 ">
+                <p className="c-fs-4">
                   Creatives and Content
                 </p>
               </div>
@@ -166,24 +163,23 @@ const affiliate = () => {
                 <h4 className="c-fs-2 col-primary c-fw-600">
                   Dedicated Affiliate Manager
                 </h4>
-                <p p className="c-fs-4 ">
+                <p className="c-fs-4 ">
                   At your service. We have an Affiliate Manager dedicated to
                   helping you promote Giddh and increase your conversions for
                   Accounting or Bookkeeping Software. He can answer your
-                  questions and work with you on strategies to sell this GST
-                  ready Accounting Software
+                  questions and work with you on strategies to sell this <a className="col-primary" href="https://giddh.com/gst.php" title="GST Accounting Software">GST ready Accounting Software</a>
                 </p>
               </div>
             </div>
           </section>
           <section className="container  d-grid gap-3 mt-4">
             <div className=" d-flex flex-column flex-md-row-reverse gap-2 align-items-center my-3">
-              <img className="mx-auto Resources-img" src="/img/affilate/newsletter.png" />
+              <img className="mx-auto Resources-img" src="/img/affilate/newsletter.png"/>
               <div className="col-12 col-md-6">
                 <h4 className="c-fs-2 col-primary c-fw-600">
                   Exclusive Affiliate Newsletter
                 </h4>
-                <p p className="c-fs-5 ">
+                <p className="c-fs-5 ">
                   News you can use. As an Accounting Software reseller, you’ll
                   receive access to a special newsletter with important product
                   updates, news on promotions. Also you will get all details on
