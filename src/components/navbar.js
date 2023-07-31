3b727c5821d6f9bcdfeb6d13f9823b3705993f74
Ashish Kushwah <ashishkushawh888@gmail.com>
2023-07-31 10:52:41 +0530

    S1T016  :about changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -186,7 +186,7 @@ const navbar = () => {
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a className="nav-link" href="about">
                   About
                 </a>
               </li>
