f63caa349e505d5d1a6d12cc7fba3382028bb6df
Ashish Yadav <ashish@walkover>
2023-08-27 15:50:57 +0530

    remove console log

--- src/components/blogs/layout.js
+++ src/components/blogs/layout.js
@@ -2,7 +2,6 @@
 // import Navigation from "./Navigation";
 
 export default function Layout({ children }) {
-    console.log("inside Layout", children);
     return (
       <div className="root">      
         <main>{children}</main>      
