b383d3e5ebc2579afdab698a8f2c6b80b51d3abd
Rashiik <rashikhandelwal269@gmail.com>
2023-08-28 17:41:48 +0530

    date-changes

--- src/components/blogs/postItem.js
+++ src/components/blogs/postItem.js
@@ -1,11 +1,9 @@
-// import Date from "./date";
-import Link from "next/link";
-// import { parseISO } from "date-fns";
-import { format } from "date-fns";
+import Date from "./date";
+import { parseISO } from "date-fns";
 
 export default function PostItem({ post }) { 
-  let date = new Date(post.date);
-    const newdate = format(date, "LLLL d, yyyy")
+  // let date = new Date(post.date);
+  //   const newdate = format(date, "LLLL d, yyyy")
   /* function calculateReadTime(articleText, wordsPerMinute = 200) {
     // Count the number of words in the article
     const words = articleText.match(/\w+/g);
@@ -19,8 +17,7 @@ export default function PostItem({ post }) {
   const readTime = calculateReadTime(article); */
   return (      
     <a className="blog-card" href={"/blog/" + post.slug}>  
-    <p>{newdate}</p>    
-        {/* <Date date={post.date} /> */}
+        <Date date={parseISO(post.date)} />
         {/* {post.thumbnail !== "" && <img src={post.thumbnail} />} */}
         <div className="title">{post.title}</div>
     </a>
