755336e41194d14faf37dee8314bf27d66051ac2
Divyanshu <divyanshushrivastava123@gmail.com>
2023-09-06 20:10:46 +0530

    working on tally plus giddh

---
+++ src/pages/tallyplusgiddh.js
@@ -0,0 +1,17 @@
+const tallyPlusGiddh = () => {
+  return (
+    <>
+      <div className="container-fluid giddh-plus-tally">
+        <div className="row">
+          <div className="col-lg-6 col-md-12">
+            <div className="giddh-plus-tally__left-wrapper"></div>
+          </div>
+          <div className="col-lg-6 col-md-12">
+            <div className="giddh-plus-tally__right-wrapper"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default tallyPlusGiddh;
