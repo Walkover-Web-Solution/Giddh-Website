2d5c30ca9768714a34bd801ee03b021a1361e6ac
Divyanshu <divyanshushrivastava123@gmail.com>
2023-09-07 20:02:48 +0530

    working on giddh vs zoho

--- src/pages/giddh-vs-zoho.js
+++ src/pages/giddh-vs-zoho.js
@@ -1,8 +1,23 @@
 const giddhVsZoho = () => {
-    return (
-        <>
-        <h1 className="col-primary my-5 py-5 text-center">Coming Soon...</h1>
-        </>
-    )
+  return (
+    <>
+      <div className="giddh-vs-zoho">
+        <section className="container-fluid giddh-vs-zoho__banner">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6 col-md-12">
+                <div className="banner-content">
+                <h1 className="banner-content__title">Zoho Books vs GIDDH</h1>
+                </div>
+              </div>
+              <div className="col-lg-6 col-md-12">
+
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 };
 export default giddhVsZoho;
