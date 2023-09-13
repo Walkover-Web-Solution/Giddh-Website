d4f25370ba7f6e23261250fd6910b1fb41af0b52
Divyanshu <divyanshushrivastava123@gmail.com>
2023-09-13 16:50:06 +0530

    compare pages content update and client summary page created

--- src/components/clientSummary.js
+++ src/components/clientSummary.js
@@ -1,54 +1,65 @@
 const clientSummary = () => {
+  const ourClient = [
+    {
+      clientName: "RackBank",
+      clientLogo: "/rackbank-logo.png",
+      websiteLink: "https://www.rackbank.com/",
+    },
+    {
+      clientName: "MSG91",
+      clientLogo: "/msg91-logo.svg",
+      websiteLink: "https://msg91.com/",
+    },
+    {
+      clientName: "Mitti",
+      clientLogo: "/mitti-logo.png",
+      websiteLink: "https://mitti.cafe/",
+    },
+    {
+      clientName: "Half Kg",
+      clientLogo: "/halfkg-logo.svg",
+      websiteLink: "https://halfkg.store/",
+    },
+  ];
 
-    const ourClient = [
-        {
-            clientName: "RackBank",
-            clientLogo: "/rackbank-logo.png",
-            websiteLink: "https://www.rackbank.com/"
-        },
-        {
-            clientName: "MSG91",
-            clientLogo: "/msg91-logo.svg",
-            websiteLink: "https://msg91.com/"
-        },
-        {
-            clientName: "Mitti",
-            clientLogo: "/mitti-logo.png",
-            websiteLink: "https://mitti.cafe/"
-        },
-        {
-            clientName: "Half Kg",
-            clientLogo: "/halfkg-logo.svg",
-            websiteLink: "https://halfkg.store/"
-        },
-    ]
-
-    return (
-        <>
-            <section className="container-fluid client-summary">
-                <div className="container">
-                    <h2 className="client-summary__title text-center col-primary">Our clients list</h2>
-                    <div className="client-summary__content-wrapper">
-                        <div className="row row-gap-5 ">
-                            {ourClient.map((res) => {
-                                return (
-                                    <>
-                                        <div className="col-lg-3 col-md-6 col-sm-12 column-wrapper">
-                                            <a href={res.websiteLink} target="_blank" className="d-block client">
-                                                <figure>
-                                                    <img src={"/img/our-clients" + res.clientLogo} alt={res.clientName} width="auto" height="60" />
-                                                </figure>
-                                            </a>
-                                        </div>
-                                    </>
-                                )
-                            })}
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <section className="container-fluid client-summary">
+        <div className="container">
+          <h2 className="client-summary__title text-center col-primary">
+            Our top clients
+          </h2>
+          <div className="client-summary__content-wrapper">
+            <div className="row row-gap-5 ">
+              {ourClient.map((res) => {
+                return (
+                  <div
+                    key={res.clientName}
+                    className="col-lg-3 col-md-6 col-sm-12 column-wrapper"
+                  >
+                    <a
+                      href={res.websiteLink}
+                      target="_blank"
+                      className="d-block client"
+                    >
+                      <figure>
+                        <img
+                          src={"/img/our-clients" + res.clientLogo}
+                          alt={res.clientName}
+                          width="auto"
+                          height="60"
+                        />
+                      </figure>
+                    </a>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default clientSummary;
