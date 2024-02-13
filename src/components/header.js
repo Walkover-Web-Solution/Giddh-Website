b3a4c39b5a1d0c12354c86b49aded1357aa7744a
Ashish Yadav <ashish@walkover>
2024-02-13 15:11:29 +0530

    add hreflang:1

--- src/components/header.js
+++ src/components/header.js
@@ -9,6 +9,17 @@ const header = (props) => {
   const [restrictFromSeo, setRestrictFromSeo] = useState(false);
   const countryList = ["", "in", "ae", "uk"];
   const country = link?.linkPrefix?.replace('/','');
+  
+  const split = currentPath.split('/');
+  var page = '';
+  
+  if(split.length === 2){
+    page = split[1].length === 2 ? '' : `/${split[1]}`;
+  }
+  if(split.length === 3){
+    page = `/${split[2]}`;
+  }
+  
   if(countryList.includes(country)){
     var hreflang;
 
@@ -37,8 +48,11 @@ const header = (props) => {
         { metaData?.keywords && <meta name="keywords" content={ metaData.keywords }></meta> }
         <link rel="icon" type="image/x-icon" href="/favico.svg"></link>
         <link rel="canonical" href={`https://giddh.com${currentPath}`} />
-        <link rel="alternate" hrefLang="x-default" href={`https://giddh.com${currentPath}`} />
-        <link rel="alternate" hrefLang={hreflang} href={`https://giddh.com${currentPath}`} />
+        <link rel="alternate" hrefLang="x-default" href={`https://giddh.com${page}`} />
+        <link rel="alternate" hrefLang="en-IN" href={`https://giddh.com/in${page}`} />
+        <link rel="alternate" hrefLang="en-GB" href={`https://giddh.com/uk${page}`} />
+        <link rel="alternate" hrefLang="en-AE" href={`https://giddh.com/ae${page}`} />
+        <link rel="alternate" hrefLang="en-US" href={`https://giddh.com/us${page}`} />
 
         {restrictFromSeo && <meta name="robots" content="noindex, nofollow" />}
       </Head>
