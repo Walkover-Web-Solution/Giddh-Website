9806c3890b4cdce6c597c6984daace141e26f108
Ravinder <ravinder@walkover.in>
2024-02-13 15:44:41 +0530

    code ref

--- src/components/header.js
+++ src/components/header.js
@@ -3,60 +3,52 @@ import Data from "@/data/metadata.json";
 import { useEffect, useState } from "react";
 
 const header = (props) => {
-  const link = props.path;
-  const currentPath = props.browserPath
-  const metaData = Data[currentPath];
-  const [restrictFromSeo, setRestrictFromSeo] = useState(false);
-  const countryList = ["", "in", "ae", "uk"];
-  const country = link?.linkPrefix?.replace('/','');
-  
-  const split = currentPath.split('/');
-  var page = '';
-  
-  if(split.length === 2){
-    page = split[1].length === 2 ? '' : `/${split[1]}`;
-  }
-  if(split.length === 3){
-    page = `/${split[2]}`;
-  }
-  
-  if(countryList.includes(country)){
-    var hreflang;
+    const link = props.path;
+    const currentPath = props.browserPath
+    const metaData = Data[currentPath];
+    const [restrictFromSeo, setRestrictFromSeo] = useState(false);
+    const countryList = ["", "in", "ae", "uk"];
+    const country = link?.linkPrefix?.replace('/', '');
 
-    if(country === 'in'){
-        hreflang = 'en-IN';
-    } else if(country === 'uk'){
-        hreflang = 'en-GB';
-    } else if(country === 'ae'){
-        hreflang = 'en-AE';
-    } else {
-        hreflang = 'en-US';
+    const path = currentPath.split('/');
+    const page = (path && path.length > 2) ? '/' + path[2] : '';
+
+    if (countryList.includes(country)) {
+        var hreflang;
+
+        if (country === 'in') {
+            hreflang = 'en-IN';
+        } else if (country === 'uk') {
+            hreflang = 'en-GB';
+        } else if (country === 'ae') {
+            hreflang = 'en-AE';
+        } else {
+            hreflang = 'en-US';
+        }
     }
-  }
 
-  useEffect(() => {
-    const isRestrictedPage =
-      window.location.href && window.location.href.indexOf("v2/login") > -1;
-    setRestrictFromSeo(isRestrictedPage);
-  }, []);
+    useEffect(() => {
+        const isRestrictedPage = window.location.href && window.location.href.indexOf("v2/login") > -1;
+        setRestrictFromSeo(isRestrictedPage);
+    }, []);
 
-  return (
-    <>
-      <Head>
-        <title>{metaData?.title}</title>
-        { metaData?.description && <meta name="description" content={metaData.description}></meta>}
-        { metaData?.keywords && <meta name="keywords" content={ metaData.keywords }></meta> }
-        <link rel="icon" type="image/x-icon" href="/favico.svg"></link>
-        <link rel="canonical" href={`https://giddh.com${currentPath}`} />
-        <link rel="alternate" hrefLang="x-default" href={`https://giddh.com${page}`} />
-        <link rel="alternate" hrefLang="en-IN" href={`https://giddh.com/in${page}`} />
-        <link rel="alternate" hrefLang="en-GB" href={`https://giddh.com/uk${page}`} />
-        <link rel="alternate" hrefLang="en-AE" href={`https://giddh.com/ae${page}`} />
-        <link rel="alternate" hrefLang="en-US" href={`https://giddh.com/us${page}`} />
+    return (
+        <>
+            <Head>
+                <title>{metaData?.title}</title>
+                {metaData?.description && <meta name="description" content={metaData.description}></meta>}
+                {metaData?.keywords && <meta name="keywords" content={metaData.keywords}></meta>}
+                <link rel="icon" type="image/x-icon" href="/favico.svg"></link>
+                <link rel="canonical" href={`https://giddh.com${currentPath}`} />
+                <link rel="alternate" hrefLang="x-default" href={`https://giddh.com${page}`} />
+                <link rel="alternate" hrefLang="en-IN" href={`https://giddh.com/in${page}`} />
+                <link rel="alternate" hrefLang="en-GB" href={`https://giddh.com/uk${page}`} />
+                <link rel="alternate" hrefLang="en-AE" href={`https://giddh.com/ae${page}`} />
+                <link rel="alternate" hrefLang="en-US" href={`https://giddh.com/us${page}`} />
 
-        {restrictFromSeo && <meta name="robots" content="noindex, nofollow" />}
-      </Head>
-    </>
-  );
+                {restrictFromSeo && <meta name="robots" content="noindex, nofollow" />}
+            </Head>
+        </>
+    );
 };
 export default header;
