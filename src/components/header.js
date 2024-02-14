1c46d7ba5bb0104af90d00acc4f74dc00b71e3a8
piyusssshh <piyush@whozzat.com>
2024-02-14 13:49:44 +0530

    PIYUSSSSHH | hrefLang | hrefLang tag added for pages & bug fixed

--- src/components/header.js
+++ src/components/header.js
@@ -3,52 +3,94 @@ import Data from "@/data/metadata.json";
 import { useEffect, useState } from "react";
 
 const header = (props) => {
-    const link = props.path;
-    const currentPath = props.browserPath
-    const metaData = Data[currentPath];
-    const [restrictFromSeo, setRestrictFromSeo] = useState(false);
-    const countryList = ["", "in", "ae", "uk"];
-    const country = link?.linkPrefix?.replace('/', '');
+  const link = props.path;
+  const currentPath = props.browserPath;
+  const metaData = Data[currentPath];
+  const [restrictFromSeo, setRestrictFromSeo] = useState(false);
+  const countryList = ["", "in", "ae", "uk"];
+  const country = link?.linkPrefix?.replace("/", "");
 
-    const path = currentPath.split('/');
-    const page = (path && path.length > 2) ? '/' + path[2] : '';
+  const pageNotInGlobal = ["/gst", "/vat", "/e-invoice", "/tallyplusgiddh"];
+  const pageNotInIndia = ["/vat", "/blog"];
+  const pageNotInUK = ["/gst", "/e-invoice", "/tallyplusgiddh", "/blog"];
+  const pageNotInAE = ["/gst", "/e-invoice", "/tallyplusgiddh", "/blog"];
 
-    if (countryList.includes(country)) {
-        var hreflang;
-
-        if (country === 'in') {
-            hreflang = 'en-IN';
-        } else if (country === 'uk') {
-            hreflang = 'en-GB';
-        } else if (country === 'ae') {
-            hreflang = 'en-AE';
-        } else {
-            hreflang = 'en-US';
-        }
+  var pathPage, pathCountry, isOnlyGlobal;
+  const pathArr = currentPath.split("/");
+  if (pathArr.length == 2) {
+    if (countryList.includes(pathArr[1])) {
+      pathPage = "";
+      pathCountry = "/" + pathArr[1];
+    } else {
+      pathPage = "/" + pathArr[1];
+      pathCountry = "";
     }
+  } else {
+    if (countryList.includes(pathArr[1])) {
+      pathCountry = "/" + pathArr[1];
+      pathPage = "/" + pathArr.slice(2).join("/");
+    } else {
+      pathPage = "/" + pathArr.slice(1).join("/");
+      pathCountry = "";
+      isOnlyGlobal = pathArr[1] === "blog";
+    }
+  }
 
-    useEffect(() => {
-        const isRestrictedPage = window.location.href && window.location.href.indexOf("v2/login") > -1;
-        setRestrictFromSeo(isRestrictedPage);
-    }, []);
+  useEffect(() => {
+    const isRestrictedPage =
+      window.location.href && window.location.href.indexOf("v2/login") > -1;
+    setRestrictFromSeo(isRestrictedPage);
+  }, []);
 
-    return (
-        <>
-            <Head>
-                <title>{metaData?.title}</title>
-                {metaData?.description && <meta name="description" content={metaData.description}></meta>}
-                {metaData?.keywords && <meta name="keywords" content={metaData.keywords}></meta>}
-                <link rel="icon" type="image/x-icon" href="/favico.svg"></link>
-                <link rel="canonical" href={`https://giddh.com${currentPath}`} />
-                <link rel="alternate" hrefLang="x-default" href={`https://giddh.com${page}`} />
-                <link rel="alternate" hrefLang="en-IN" href={`https://giddh.com/in${page}`} />
-                <link rel="alternate" hrefLang="en-GB" href={`https://giddh.com/uk${page}`} />
-                <link rel="alternate" hrefLang="en-AE" href={`https://giddh.com/ae${page}`} />
-                <link rel="alternate" hrefLang="en-US" href={`https://giddh.com/us${page}`} />
+  return (
+    <>
+      <Head>
+        <title>{metaData?.title}</title>
+        {metaData?.description && (
+          <meta name='description' content={metaData.description}></meta>
+        )}
+        {metaData?.keywords && (
+          <meta name='keywords' content={metaData.keywords}></meta>
+        )}
+        <link rel='icon' type='image/x-icon' href='/favico.svg'></link>
+        <link rel='canonical' href={`https://giddh.com${currentPath}`} />
+        {!pageNotInGlobal.includes(pathPage) && (
+          <link
+            rel='alternate'
+            hrefLang='x-default'
+            href={`https://giddh.com${pathPage}`}
+          />
+        )}
+        {!isOnlyGlobal && (
+          <>
+            {!pageNotInIndia.includes(pathPage) && (
+              <link
+                rel='alternate'
+                hrefLang='en-IN'
+                href={`https://giddh.com/in${pathPage}`}
+              />
+            )}
+            {!pageNotInUK.includes(pathPage) && (
+              <link
+                rel='alternate'
+                hrefLang='en-GB'
+                href={`https://giddh.com/uk${pathPage}`}
+              />
+            )}
+            {!pageNotInAE.includes(pathPage) && (
+              <link
+                rel='alternate'
+                hrefLang='en-AE'
+                href={`https://giddh.com/ae${pathPage}`}
+              />
+            )}
+          </>
+        )}
 
-                {restrictFromSeo && <meta name="robots" content="noindex, nofollow" />}
-            </Head>
-        </>
-    );
+        {restrictFromSeo && <meta name='robots' content='noindex, nofollow' />}
+      </Head>
+    </>
+  );
 };
+
 export default header;
