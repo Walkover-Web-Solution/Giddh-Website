02f66aefda3482f6ecd3e94e49e009d88d0cbc4f
Saurabh186 <sawrabhsharma1234@gmail.com>
2024-03-16 18:26:36 +0530

    SAURABH | new page create

---
+++ src/pages/windows-app-error-codes.js
@@ -0,0 +1,24 @@
+const windowsAppErrorCodes = () => {
+    return (
+        <>
+            <div className="container mt-5 py-5">
+                <h1 className="fw-bold text-center c-fs-2">Windows app error codes</h1>
+                <ul id="firstId" className="mt-4">
+                    <li className="discription" id="">
+                        <h2 className="fs-5">Heading</h2>
+                        <p>Content here ...</p>
+                    </li>
+                    <li className="discription" id="">
+                        <h2 className="fs-5">Heading</h2>
+                        <p>Content here ...</p>
+                    </li>
+                    <li className="discription" id="">
+                        <h2 className="fs-5">Heading</h2>
+                        <p>Content here ...</p>
+                    </li>
+                </ul>
+            </div>
+        </>
+    );
+};
+export default windowsAppErrorCodes;
