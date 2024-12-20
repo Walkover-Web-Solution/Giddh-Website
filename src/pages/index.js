c03e892d429e12c3c286e4cdf61c87c22e524348
Divyanshu <divyanshushrivastava123@gmail.com>
2024-12-20 12:53:47 +0530

    add create trust badge section for UK in banner page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import MakeTheSwitchIndexPage from "@/components/makeTheSwitch";
 import BannerSection from "@/components/bannerSection";
 import ClientSummary from "@/components/clientSummary";
 import FaqHomePage from "@/components/faqHomePage";
+import TrustBadge from "@/components/trust-badge";
 
 const index = (path) => {
   const link = path.path;
   return (
     <>
       <BannerSection path={link} />
+      { link.isUK && <TrustBadge /> }
       <FeatureGallery path={link} />
       <ClientSummary />
       <MakeTheSwitchIndexPage path={link} />
