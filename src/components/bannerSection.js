62fc0fe1a351ba841bd017fd23296f368dfcdade
Dilpreet Singh Dang <dilpreet@whozzat.com>
2025-01-07 11:19:16 +0530

    Dilpreet | Main | Prod - Contact sales button is not working on website on mobile resolution

--- src/components/bannerSection.js
+++ src/components/bannerSection.js
@@ -1,18 +1,34 @@
 import { useEffect, useState } from "react";
-
+import { createSlider } from "@typeform/embed";
 const bannerSection = (path) => {
   const link = path.path;
 
   const [utm, setUtm] = useState(null);
 
-  useEffect(() => {
-    const utmParams = new URLSearchParams({
-      utm_source: getLocalStorage("utm_source"),
-      utm_medium: getLocalStorage("utm_medium"),
-      utm_campaign: getLocalStorage("utm_campaign"),
-    }).toString();
-    setUtm(utmParams);
-  }, []);
+ useEffect(() => {
+   const utmParams = new URLSearchParams({
+     utm_source: getLocalStorage("utm_source"),
+     utm_medium: getLocalStorage("utm_medium"),
+     utm_campaign: getLocalStorage("utm_campaign"),
+   }).toString();
+   setUtm(utmParams);
+ }, []);
+
+    const openTypeForm = () => {
+      const slider = createSlider("https://form.typeform.com/to/uOtrQ4tb", {
+        position: "right", // Specify position ('left', 'center', 'right')
+        opacity: 83, // Background overlay opacity
+        iframeProps: {
+          title: "Accounting Software Company Contact Form", // Accessible iframe title
+        },
+        transitiveSearchParams: true, // Pass search params to Typeform
+        medium: "snippet", // Set the medium for analytics/tracking
+        hidden: {
+          utm: utm, // Pass the UTM parameters as hidden fields
+        },
+      });
+      slider.open(); // Opens the slider
+    };
 
   return (
     <>
@@ -21,24 +37,15 @@ const bannerSection = (path) => {
           <div className="row h-100 align-items-xl-start align-items-lg-center">
             <div className="col-lg-6 col-md-12">
               <h1 className="col-primary heading-text">
-                    { link.isGlobal && "Online Accounting Software For Businesses"}
-                    { link.isIndia && "Business Accounting Software India"}
-                    { link.isUK && "VAT-Compliant Accounting Software For UK Small Businesses"}
-                    { link.isAE && "Cloud-based Accounting Software For UAE Businesses"}
+                {link.isGlobal && "Online Accounting Software For Businesses"}
+                {link.isIndia && "Business Accounting Software India"}
+                {link.isUK &&
+                  "VAT-Compliant Accounting Software For UK Small Businesses"}
+                {link.isAE &&
+                  "Cloud-based Accounting Software For UAE Businesses"}
               </h1>
               <div className="banner__contact_btn mt-5">
-                <button
-                  data-tf-slider="uOtrQ4tb"
-                  data-tf-position="right"
-                  data-tf-opacity="83"
-                  data-tf-hide-headers
-                  data-tf-iframe-props="title=Accounting Software Company Contact Form"
-                  data-tf-transitive-search-params
-                  data-tf-medium="snippet"
-                  data-tf-hidden={utm}
-                >
-                  Contact Sales
-                </button>
+                <button onClick={openTypeForm}>Contact Sales</button>
               </div>
             </div>
             <div className="col-lg-6 col-md-12">
