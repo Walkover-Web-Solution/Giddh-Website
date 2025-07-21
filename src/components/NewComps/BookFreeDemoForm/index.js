6a2eb381f63c4a3b7facc065d837e6afc545d689
piyusssshh <piyush@whozzat.com>
2025-07-21 18:10:07 +0530

    PIYUSH | added segmento API

--- src/components/NewComps/BookFreeDemoForm/index.js
+++ src/components/NewComps/BookFreeDemoForm/index.js
@@ -10,6 +10,7 @@ export default function BookFreeDemoForm() {
     business: "",
   });
   const [submitted, setSubmitted] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -19,26 +20,39 @@ export default function BookFreeDemoForm() {
     }));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
+    setSubmitting(true);
     e.preventDefault();
     const dataToStore = {
-      name: formData.name,
-      email: formData.email,
-      phone: formData.phone,
-      business: formData.business,
+      input: {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        business: formData.business,
+      },
     };
-    sendDataInSegmento(dataToStore);
-    console.log("Form Data as JSON:", JSON.stringify(dataToStore));
-    setSubmitted(true);
-    setFormData({
-      name: "",
-      email: "",
-      phone: "",
-      business: "",
-    });
+    try {
+      const data = await sendDataInSegmento(dataToStore);
+      if (data?.status === "success") {
+        setSubmitted(true);
+        setFormData({
+          name: "",
+          email: "",
+          phone: "",
+          business: "",
+        });
+        setSubmitting(false);
+      } else {
+        setSubmitting(false);
+        console.error("Error submitting form:", data?.message);
+      }
+    } catch (error) {
+      setSubmitting(false);
+      console.error("Error submitting form:", error);
+    }
   };
+
   return (
-    <>
       <div
         className={`${style.form_container} w-100 bg-light-blue p-lg-5 p-md-4 p-3 d-flex align-items-center justify-content-center rounded`}
       >
@@ -101,35 +115,40 @@ export default function BookFreeDemoForm() {
               </div>
               <button
                 type="submit"
-                className="btn btn-primary w-100 "
+                className={`btn ${
+                  submitting ? "btn-disabled" : "btn-primary"
+                } w-100`}
                 aria-label="Signup for Accounting Software"
               >
-                Book Free Demo
+                {submitting ? "Sheduling..." : "Book Free Demo"}
               </button>
             </form>
           </div>
         )}
       </div>
-    </>
   );
 }
 
-function sendDataInSegmento(data) {
+async function sendDataInSegmento(data) {
   const options = {
     method: "POST",
     headers: {
       accept: "application/json",
       "content-type": "application/json",
-      authkey: process.env.MSG91_AUTH_KEY,
+      tokenAuth: process.env.NEXT_PUBLIC_MSG91_AUTH_KEY,
     },
     body: JSON.stringify(data),
   };
 
-  fetch(
-    `https://control.msg91.com/api/v5/segmento/api/v1/phonebooks/${process.env.MSG91_PHONEBOOK_ID}/contacts`,
-    options
-  )
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
+  try {
+    const response = await fetch(
+      `https://control.msg91.com/api/v5/segmento/api/v1/phonebooks/${process.env.NEXT_PUBLIC_MSG91_PHONEBOOK_ID}/contacts`,
+      options
+    );
+    const result = await response.json();
+    return result;
+  } catch (err) {
+    console.error(err);
+    return { status: "error", message: err.message };
+  }
 }
