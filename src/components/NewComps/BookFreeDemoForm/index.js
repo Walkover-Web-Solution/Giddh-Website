1052649f39a471830569446a1294b9f94d74fd9c
piyusssshh <piyush@whozzat.com>
2025-07-22 11:11:11 +0530

    PIYUSH | env updated

--- src/components/NewComps/BookFreeDemoForm/index.js
+++ src/components/NewComps/BookFreeDemoForm/index.js
@@ -53,79 +53,79 @@ export default function BookFreeDemoForm() {
   };
 
   return (
-      <div
-        className={`${style.form_container} w-100 bg-light-blue p-lg-5 p-md-4 p-3 d-flex align-items-center justify-content-center rounded`}
-      >
-        {submitted ? (
-          <div className="d-flex align-items-center justify-content-center flex-column gap-0">
-            <MdCheckCircleOutline className={style.check} />
-            <p className="col-primary c-fw-600 mb-0 c-fs-2">Thank You!</p>
-            <p className="col-primary mb-0 c-fs-4 text-center">
-              Your demo request has been submitted successfully. Our team will
-              get in touch with you shortly.
-            </p>
-          </div>
-        ) : (
-          <div className="d-flex align-items-center justify-content-center flex-column gap-3 w-100">
-            <p className="col-primary c-fw-600 mb-0 c-fs-4">
-              Explore the Advanced Accounting Features of Giddh
-            </p>
-            <form onSubmit={handleSubmit} className="w-100">
-              <div className="d-flex flex-column gap-2 input-group-lg mb-4">
-                <input
-                  type="text"
-                  name="name"
-                  aria-label="Name"
-                  placeholder="Name*"
-                  required
-                  className="form-control border-0 c-fs-5"
-                  value={formData.name}
-                  onChange={handleChange}
-                />
-                <input
-                  type="email"
-                  name="email"
-                  aria-label="Email"
-                  placeholder="Email*"
-                  required
-                  className="form-control border-0 c-fs-5"
-                  value={formData.email}
-                  onChange={handleChange}
-                />
-                <input
-                  type="tel"
-                  name="phone"
-                  aria-label="Phone Number"
-                  placeholder="Contact No."
-                  required
-                  className="form-control border-0 c-fs-5"
-                  value={formData.phone}
-                  onChange={handleChange}
-                />
-                <input
-                  type="text"
-                  name="business"
-                  aria-label="Business name"
-                  placeholder="Business Name"
-                  required
-                  className="form-control border-0 c-fs-5"
-                  value={formData.business}
-                  onChange={handleChange}
-                />
-              </div>
-              <button
-                type="submit"
-                className={`btn ${
-                  submitting ? "btn-disabled" : "btn-primary"
-                } w-100`}
-                aria-label="Signup for Accounting Software"
-              >
-                {submitting ? "Scheduling..." : "Book Free Demo"}
-              </button>
-            </form>
-          </div>
-        )}
-      </div>
+    <div
+      className={`${style.form_container} w-100 bg-light-blue p-lg-5 p-md-4 p-3 d-flex align-items-center justify-content-center rounded`}
+    >
+      {submitted ? (
+        <div className="d-flex align-items-center justify-content-center flex-column gap-0">
+          <MdCheckCircleOutline className={style.check} />
+          <p className="col-primary c-fw-600 mb-0 c-fs-2">Thank You!</p>
+          <p className="col-primary mb-0 c-fs-4 text-center">
+            Your demo request has been submitted successfully. Our team will get
+            in touch with you shortly.
+          </p>
+        </div>
+      ) : (
+        <div className="d-flex align-items-center justify-content-center flex-column gap-3 w-100">
+          <p className="col-primary c-fw-600 mb-0 c-fs-4">
+            Explore the Advanced Accounting Features of Giddh
+          </p>
+          <form onSubmit={handleSubmit} className="w-100">
+            <div className="d-flex flex-column gap-2 input-group-lg mb-4">
+              <input
+                type="text"
+                name="name"
+                aria-label="Name"
+                placeholder="Name*"
+                required
+                className="form-control border-0 c-fs-5"
+                value={formData.name}
+                onChange={handleChange}
+              />
+              <input
+                type="email"
+                name="email"
+                aria-label="Email"
+                placeholder="Email*"
+                required
+                className="form-control border-0 c-fs-5"
+                value={formData.email}
+                onChange={handleChange}
+              />
+              <input
+                type="tel"
+                name="phone"
+                aria-label="Phone Number"
+                placeholder="Contact No."
+                required
+                className="form-control border-0 c-fs-5"
+                value={formData.phone}
+                onChange={handleChange}
+              />
+              <input
+                type="text"
+                name="business"
+                aria-label="Business name"
+                placeholder="Business Name"
+                required
+                className="form-control border-0 c-fs-5"
+                value={formData.business}
+                onChange={handleChange}
+              />
+            </div>
+            <button
+              type="submit"
+              className={`btn ${
+                submitting ? "btn-disabled" : "btn-primary"
+              } w-100`}
+              aria-label="Signup for Accounting Software"
+            >
+              {submitting ? "Scheduling..." : "Book Free Demo"}
+            </button>
+          </form>
+        </div>
+      )}
+    </div>
   );
 }
 
@@ -142,7 +142,7 @@ async function sendDataInSegmento(data) {
 
   try {
     const response = await fetch(
-      `https://control.msg91.com/api/v5/segmento/api/v1/phonebooks/${process.env.NEXT_PUBLIC_MSG91_PHONEBOOK_ID}/contacts`,
+      `${process.env.NEXT_PUBLIC_MSG91_PHONEBOOK_URL}`,
       options
     );
     const result = await response.json();
