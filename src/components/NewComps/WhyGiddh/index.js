cc39ae05fa272d4da4bab68430d6ef97bc75cbcf
Isha Dubey <ishadubey36@gmail.com>
2025-09-16 12:04:31 +0530

    Isha | bug fixes

--- src/components/NewComps/WhyGiddh/index.js
+++ src/components/NewComps/WhyGiddh/index.js
@@ -12,61 +12,34 @@ export default function WhyGiddh() {
   return (
     <section className="container py-4 mt-5 mb-5 outfit-font">
       <h1 className="text-center mb-4 cactus-font">{data.heading}</h1>
-      <p className="text-center mb-4 w-75 mx-auto ">{data.subheading}</p>
-
-      <div className="bg-white border rounded overflow-hidden d-none d-md-block">
-        <div className="row border-bottom">
-          <div className="col-md-4 px-4 py-3 fw-semibold text-dark border-end">
-            Feature
-          </div>
-          <div className="col-md-4 px-4 py-3 fw-semibold text-dark border-end">
-            Other Accounting Software
-          </div>
-          <div className="col-md-4 px-4 py-3 fw-semibold text-dark">
-            Giddh Accounting Software
-          </div>
+      <p className="text-center mb-4 w-75 mx-auto">{data.subheading}</p>
+      <div className="bg-white border rounded overflow-hidden">
+        <div className="row border-bottom fw-semibold text-dark text-center">
+          <div className="col-6 col-md-4 px-3 py-2 border-end">Feature</div>
+          <div className="col-3 col-md-4 px-3 py-2 border-end">Other</div>
+          <div className="col-3 col-md-4 px-3 py-2">Giddh</div>
         </div>
-
         {whyGiddhData.map((row, index) => (
-          <div key={index} className="row border-bottom ">
-            <div className="col-md-4 px-4 py-3 border-end">{row["features"]}</div>
-            <div className="col-md-4 px-4 py-3 border-end">
-              {row["Other Accounting Software"] === "true" ? <MdDone size={26} className="text-success" /> : <MdClose size={26} className="text-danger" />}
+          <div key={index} className="row border-bottom text-center">
+            <div className="col-6 col-md-4 px-3 py-2 border-end ">
+              {row["features"]}
             </div>
-            <div className="col-md-4 px-4 py-3">
-              {row["Giddh Accounting Software"] === "true" ? <MdDone size={26} className="text-success" /> : <MdClose size={26} className="text-danger" />}
+            <div className="col-3 col-md-4 px-3 py-2 border-end">
+              {row["Other Accounting Software"] === "true" ? (
+                <MdDone size={20} className="text-success" />
+              ) : (
+                <MdClose size={20} className="text-danger" />
+              )}
+            </div>
+            <div className="col-3 col-md-4 px-3 py-2">
+              {row["Giddh Accounting Software"] === "true" ? (
+                <MdDone size={20} className="text-success" />
+              ) : (
+                <MdClose size={20} className="text-danger" />
+              )}
             </div>
           </div>
         ))}
-      </div>
-      <div className="d-block d-md-none overflow-y-auto">
-        <div className="bg-white border rounded">
-          <div className="row border-bottom fw-semibold text-dark c-fs-5">
-            <div className="col-4 px-2 py-2 ps-3 border-end">Feature</div>
-            <div className="col-4 px-2 py-2 border-end">Other</div>
-            <div className="col-4 px-2 py-2">Giddh</div>
-          </div>
-
-          {whyGiddhData.map((row, index) => (
-            <div key={index} className="row border-bottom c-fs-6">
-              <div className="col-4 px-2 py-2 border-end ps-3 overflow-hidden">{row["features"]}</div>
-              <div className="col-4 px-2 py-2 border-end">
-                {row["Other Accounting Software"] === "true" ? (
-                  <MdDone size={20} className="text-success" />
-                ) : (
-                  <MdClose size={20} className="text-danger" />
-                )}
-              </div>
-              <div className="col-4 px-2 py-2">
-                {row["Giddh Accounting Software"] === "true" ? (
-                  <MdDone size={20} className="text-success" />
-                ) : (
-                  <MdClose size={20} className="text-danger" />
-                )}
-              </div>
-            </div>
-          ))}
-        </div>
       </div>
     </section>
   );
