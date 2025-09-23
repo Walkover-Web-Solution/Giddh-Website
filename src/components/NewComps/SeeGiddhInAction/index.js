935b6e9f6a4e323299de02cb465073e2471ebb2b
piyusssshh <piyush@whozzat.com>
2025-09-23 13:10:55 +0530

    PIYUSH | images and contnt update

--- src/components/NewComps/SeeGiddhInAction/index.js
+++ src/components/NewComps/SeeGiddhInAction/index.js
@@ -1,14 +1,15 @@
 import BookFreeDemoForm from "../BookFreeDemoForm";
+import styles from './SeeGiddhInAction.module.scss'
 
 export default function SeeGiddhInAction() {
   return (
-    <section id="SeeGiddhInAction">
+    <section id="SeeGiddhInAction" className={styles.section}>
       <div className="section_py container d-flex gap-2 gap-md-5 w-100 align-items-center justify-content-center flex-column flex-md-row">
         <div className="d-flex flex-column gap-3 col-md-5 col-lg-6">
           <div className="content-width">
-            <h1 className="heading col-primary cactus-font">
+            <h2 className="heading col-primary cactus-font">
               Giddh For All Your Accounting Needs
-            </h1>
+            </h2>
             <p className="c-fs-4 outfit-font">
               Manage 100 companies, unlimited users, real-time reports, GST & VAT compliance, and more—streamline accounting and inventory in one platform.
             </p>
