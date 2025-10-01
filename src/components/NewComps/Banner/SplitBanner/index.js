2051d8c46b2262ab1bb934de7a9451c29272d245
piyusssshh <piyush@whozzat.com>
2025-10-01 12:02:07 +0530

    PIYUSH | CTA updated

--- src/components/NewComps/Banner/SplitBanner/index.js
+++ src/components/NewComps/Banner/SplitBanner/index.js
@@ -5,19 +5,20 @@ import Image from "next/image";
 export default function SplitBanner({ compdata }) {
   return (
     <>
-      <section className={`py-3 outfit-font gradient ${!compdata?.header ? "padding-nav" : ""}`}>
+      <section
+        className={`py-3 outfit-font gradient ${
+          !compdata?.header ? "padding-nav" : ""
+        }`}
+      >
         <div className="container px-4 py-5">
           {compdata?.header && (
             <div className="mb-5 pb-3 d-flex justify-content-between align-items-center">
-              <div className="w-100 text-start">
-                <Image
-                  src="/img/giddh-logo.svg"
-                  alt="Giddh Logo"
-                  width={120}
-                  height={24}
-                  className="text-start img-fluid fit-content"
-                />
-              </div>
+              <Image
+                src="/img/giddh-logo.svg"
+                alt="Giddh Logo"
+                width={120}
+                height={24}
+              />
               <button
                 onClick={() =>
                   document.getElementById("SeeGiddhInAction")?.scrollIntoView({
@@ -25,35 +26,52 @@ export default function SplitBanner({ compdata }) {
                     block: "center",
                   })
                 }
-                className="btn btn-primary-outline w-fit"
+                className="btn btn-primary-outline"
               >
                 Get Started
               </button>
             </div>
           )}
-          <div className={`row align-items-center ${!compdata?.form && !compdata?.image ? "justify-content-center" : "justify-content-between"} garmond-font`}>
-            <div className={`col-lg-6 col-md-6 col-12 mb-4 mb-lg-0 d-flex flex-column gap-2 ${!compdata?.form && !compdata?.image ? "pe-0 text-center col-lg-8 mx-auto" : "pe-3"}`}>
+          <div
+            className={`row align-items-center ${
+              !compdata?.form && !compdata?.image
+                ? "justify-content-center"
+                : "justify-content-between"
+            } garmond-font`}
+          >
+            <div
+              className={`col-lg-6 col-md-6 col-12 mb-4 mb-lg-0 d-flex flex-column gap-2 ${
+                !compdata?.form && !compdata?.image
+                  ? "pe-0 text-center col-lg-8 mx-auto"
+                  : "pe-3"
+              }`}
+            >
               {compdata?.tagline && (
                 <span className="font-md outfit-font">
                   {" "}
                   BUILD FOR <span className="font-primary"> SMBs</span>
-                </span>)}
-              <h1 className="font-heading font-primary">
-                {compdata?.heading}
-              </h1>
-              <p className="font-grey outfit-font">
-                {compdata?.subHeading}
-              </p>
-              <div className={`d-flex flex-wrap align-items-center gap-4 ${!compdata?.form && !compdata?.image ? "justify-content-center" : ""}`}>
-                {Array.isArray(trustBadgeBrands) && trustBadgeBrands?.map((logo, index) => (
-                  <a key={index} href={logo.href} rel="noopener noreferrer" target="_blank">
-                    <img
-                      src={logo.src}
-                      alt={logo.alt}
-                      height={34}
-                    />
-                  </a>
-                ))}
+                </span>
+              )}
+              <h1 className="font-heading font-primary">{compdata?.heading}</h1>
+              <p className="font-grey outfit-font">{compdata?.subHeading}</p>
+              <div
+                className={`d-flex flex-wrap align-items-center gap-4 ${
+                  !compdata?.form && !compdata?.image
+                    ? "justify-content-center"
+                    : ""
+                }`}
+              >
+                {Array.isArray(trustBadgeBrands) &&
+                  trustBadgeBrands?.map((logo, index) => (
+                    <a
+                      key={index}
+                      href={logo.href}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      <img src={logo.src} alt={logo.alt} height={34} />
+                    </a>
+                  ))}
               </div>
             </div>
             {compdata?.form && (
@@ -61,7 +79,13 @@ export default function SplitBanner({ compdata }) {
             )}
             {!compdata?.form && compdata?.image && (
               <div className="col-lg-6 col-md-6 col-12 text-center">
-                <Image src={compdata?.image} alt={compdata?.heading} width={600} height={700} className="w-100" />
+                <Image
+                  src={compdata?.image}
+                  alt={compdata?.heading}
+                  width={600}
+                  height={700}
+                  className="w-100"
+                />
               </div>
             )}
           </div>
--- src/components/NewComps/CTA/index.js
+++ src/components/NewComps/CTA/index.js
@@ -4,54 +4,49 @@ import { MdArrowForward } from "react-icons/md";
 
 export default function CTA({ compData, hasImage }) {
   return (
-    <section className={`${compData?.theme === "light" ? "bg-accent-light font-dark" : "bg-accent font-white position-relative"} position-relative pt-5`}>
-      <div
-        className={`container ${styles.inner} ${!hasImage ? styles.noImage : ""} d-grid align-items-center justify-content-center`}
-      >
-        <div className="d-flex  flex-column align-items-center justify-content-center gap-2 pb-5">
-          <h2
-            className={`${!hasImage && "text-center"
-              } font-400 font-sub-heading garmond-font text-center mx-auto`}
-          >
+    <section
+      className={`${
+        compData?.theme === "light"
+          ? "bg-accent-light font-dark"
+          : "bg-accent font-white outfit-font"
+      } pt-5 pb-5 pb-md-0 text-center text-md-start`}
+    >
+      <div className={`container ${!hasImage ? styles.noImage : "d-flex"}`}>
+        <div className="d-flex justify-content-center flex-column">
+          <h2 className={`${!hasImage && ""} font-sub-heading garmond-font`}>
             {compData?.heading}
           </h2>
-          <p
-            className={`${!hasImage && "text-center"
-              } font-md font-white text-center font-600`}
-          >
-            {compData?.subheading}
-          </p>
+          <p className={`${!hasImage && ""} `}>{compData?.subheading}</p>
           <div>
-            {Array.isArray(compData?.buttons) && compData?.buttons.map((button, index) => (
-              <a key={index} href={button?.link} target="_blank">
-                <button
-                  onClick={() =>
-                    document
-                      .getElementById("SeeGiddhInAction")
-                      ?.scrollIntoView({
-                        behavior: "smooth",
-                        block: "center",
-                      })
-                  }
-                  className={`btn font-600 ${button?.type}`}
-                >
-                  {button.text} {button.arrow && <MdArrowForward />}
-                </button>
-              </a>
-            ))}
+            {Array.isArray(compData?.buttons) &&
+              compData?.buttons.map((button, index) => (
+                <a key={index} href={button?.link} target="_blank">
+                  <button
+                    onClick={() =>
+                      document
+                        .getElementById("SeeGiddhInAction")
+                        ?.scrollIntoView({
+                          behavior: "smooth",
+                          block: "center",
+                        })
+                    }
+                    className={`${button?.type} btn`}
+                  >
+                    {button.text} {button.arrow && <MdArrowForward />}
+                  </button>
+                </a>
+              ))}
           </div>
         </div>
         {hasImage && (
-          <div className={styles.imageWrap}>
-            <Image
-              src={compData?.image}
-              alt="Person holding laptop"
-              width={320}
-              height={320}
-              priority
-              className="object-fit-contain w-100 h-auto"
-            />
-          </div>
+          <Image
+            src={compData?.image}
+            alt="Person holding laptop"
+            width={320}
+            height={320}
+            priority
+            className="w-75 d-none d-md-block"
+          />
         )}
       </div>
     </section>
