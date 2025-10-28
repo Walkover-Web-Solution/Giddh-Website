9a8d66744fb575babcbdc856a282369046f4c3b2
Isha <ishadubey36@gmail.com>
2025-10-28 15:55:05 +0530

    Isha | Broken links technical audits

--- src/pages/in/cloud-accounting-software.js
+++ src/pages/in/cloud-accounting-software.js
@@ -13,7 +13,7 @@ const OldCloudAccountingSoftware = (path) => {
   const isIndia = linkPath.isIndia;
   const isAE = linkPath.isAE;
   const isUK = linkPath.isUK;
-  const link = linkPath.linkPrefix;
+  const link = linkPath.path.linkPrefix;
 
   useEffect(() => {
     setFaq(faqs["cloud-accounting-software"]);
