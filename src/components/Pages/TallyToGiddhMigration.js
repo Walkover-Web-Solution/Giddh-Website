88f42b2af213aef1c22de19a0f895ba83aeec075
Isha <ishadubey36@gmail.com>
2025-11-13 14:59:49 +0530

    Isha | tally to giddh migration page done

--- src/components/Pages/TallyToGiddhMigration.js
+++ src/components/Pages/TallyToGiddhMigration.js
@@ -1,3 +1,19 @@
+import SplitBanner from "../NewComps/Banner/SplitBanner";
+import CTA from "../NewComps/CTA";
+import FAQs from "../NewComps/FAQs";
+import EssentialFeatures from "../NewComps/Features/EssentialFeatures";
+import QuickStartSection from "../NewComps/QuickStartSection";
+import SupportedPlatforms from "../NewComps/SupportedPlatforms";
+
 export default function TallyToGiddhMigration({ pageData }) {
-  return <></>;
+  return (
+    <>
+      <SplitBanner compData={pageData?.splitBanner} />
+      <EssentialFeatures compData={pageData?.features} />
+      <CTA compData={pageData?.cta} />
+      <QuickStartSection compData={pageData?.quickStartSection} />
+      <SupportedPlatforms supportedPlatforms={pageData?.supportedPlatforms} />
+      <FAQs faqs={pageData?.faqs} />
+    </>
+  );
 }
