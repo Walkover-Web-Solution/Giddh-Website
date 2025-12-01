3e0ab824ad79b968c2d60d1b4eff7b2598fe75ca
Isha <ishadubey36@gmail.com>
2025-12-01 15:52:29 +0530

    Isha | Landing page Changes

--- src/components/Pages/TallyToGiddhMigration.js
+++ src/components/Pages/TallyToGiddhMigration.js
@@ -2,6 +2,7 @@ import SplitBanner from "../NewComps/Banner/SplitBanner";
 import CTA from "../NewComps/CTA";
 import FAQs from "../NewComps/FAQs";
 import EssentialFeatures from "../NewComps/Features/EssentialFeatures";
+import GridFeatures from "../NewComps/Features/GridFeatures";
 import QuickStartSection from "../NewComps/QuickStartSection";
 import SupportedPlatforms from "../NewComps/SupportedPlatforms";
 
@@ -9,7 +10,7 @@ export default function TallyToGiddhMigration({ pageData }) {
   return (
     <>
       <SplitBanner compData={pageData?.splitBanner} />
-      <EssentialFeatures compData={pageData?.features} />
+      <GridFeatures features={pageData?.characteristics} />
       <CTA compData={pageData?.cta} />
       <QuickStartSection compData={pageData?.quickStartSection} />
       <SupportedPlatforms supportedPlatforms={pageData?.supportedPlatforms} />
