6306b9c8bc06d53cf72a7194a9da24e19abafb26
Isha <ishadubey36@gmail.com>
2025-12-09 12:07:20 +0530

    Isha | resolved Changes

--- src/pages/in/pricing.js
+++ src/pages/in/pricing.js
@@ -64,7 +64,6 @@ const InPricing = (path) => {
 
     fetchData(getRegionByLink());
     if (PricingData) {
-      console.log(country);
       setPricingData(PricingData[country]);
     }
   }, [PricingData]);
