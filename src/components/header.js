23278b6694480d8d02dfd201a58dfad2a9fe24fe
Isha <ishadubey36@gmail.com>
2026-01-27 14:04:44 +0530

    Isha | resolved changes - meta image

--- src/components/header.js
+++ src/components/header.js
@@ -110,9 +110,7 @@ export default function Header(props) {
           />
         )}
         {metaData?.title && <meta property="og:type" content="website" />}
-      {metaData?.image && (
-        <meta property="og:image" content={metaData.image || "/img/giddh-home-page.png"}/>
-      )}
+        <meta property="og:image" content={metaData?.image || "/img/giddh-home-page.png"}/>
       </Head>
     </>
   );
