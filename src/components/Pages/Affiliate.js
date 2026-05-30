1ed76cdf40320e91626341a7c54042c5624de2b9
isha-dubey <ishadubey36@gmail.com>
2026-05-30 14:32:50 +0530

    Fix : content fix changes

--- src/components/Pages/Affiliate.js
+++ src/components/Pages/Affiliate.js
@@ -1,10 +1,10 @@
 import SplitBanner from "../NewComps/Banner/SplitBanner";
 import AlternateFeatures from "../NewComps/Features/AlternateFeatures";
 import Stepper from "../NewComps/Stepper";
-import GridFeatures from "../NewComps/Features/GridFeatures";
 import WhyGiddh from "../NewComps/WhyGiddh";
 import CTA from "../NewComps/CTA";
 import FAQs from "../NewComps/FAQs";
+import Image from "next/image";
 
 export default function Affiliate({ pageData }) {
   return (
@@ -12,10 +12,33 @@ export default function Affiliate({ pageData }) {
       <SplitBanner compData={pageData?.splitBanner} />
       <AlternateFeatures features={pageData?.features} />
       <Stepper compData={pageData?.stepper} />
-      <GridFeatures features={pageData?.resources} />
+      {pageData?.commissionPayoutTerms && (
+        <section className="py-5 outfit-font">
+          <div className="container">
+            <div className="d-flex flex-column border rounded-4 px-3 py-4 text-center justify-content-center border-color-primary">
+              {pageData?.commissionPayoutTerms?.icon && (
+                <Image
+                  width={80}
+                  height={70}
+                  src={pageData.commissionPayoutTerms.icon}
+                  alt={pageData?.commissionPayoutTerms?.heading}
+                  className="align-self-center mb-2"
+                  loading="lazy"
+                />
+              )}
+              <p className="m-0 font-lg font-600 font-primary">
+                {pageData?.commissionPayoutTerms?.heading}
+              </p>
+              <p className="font-md font-slate-grey mb-0 mt-2 mx-auto col-lg-10">
+                {pageData?.commissionPayoutTerms?.description}
+              </p>
+            </div>
+          </div>
+        </section>
+      )}
       <WhyGiddh compData={pageData?.whyGiddh} />
-      <FAQs faqs={pageData?.faqs} />
       <CTA compData={pageData?.cta} />
+      <FAQs faqs={pageData?.faqs} />
     </>
   );
 }
