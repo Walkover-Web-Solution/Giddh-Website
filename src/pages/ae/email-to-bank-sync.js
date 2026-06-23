66a7f148ceda71d82c309fcdfa29bafa605c0b27
isha-dubey <ishadubey36@gmail.com>
2026-06-23 16:03:55 +0530

    Fix : Added new features

---
+++ src/pages/ae/email-to-bank-sync.js
@@ -0,0 +1,5 @@
+import EmailToBankSyncComp from "@/components/Pages/EmailToBankSync";
+
+export default function AeEmailToBankSync({ pageData }) {
+  return <EmailToBankSyncComp pageData={pageData} />;
+}
---
+++ src/pages/uk/email-to-bank-sync.js
@@ -0,0 +1,5 @@
+import EmailToBankSyncComp from "@/components/Pages/EmailToBankSync";
+
+export default function UkEmailToBankSync({ pageData }) {
+  return <EmailToBankSyncComp pageData={pageData} />;
+}
