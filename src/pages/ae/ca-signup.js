1451396634a32a2531adefe9c59f1d3506210106
pallavi-mourya <pallavi@whozzat.com>
2026-09-14 15:41:43 +0530

    Pallavi | create a landing page specifically for ICAI members and its corresponding QR code

---
+++ src/pages/ae/ca-signup.js
@@ -0,0 +1,11 @@
+import CaSignup from "@/pages/ca-signup";
+
+const aeCaSignup = (path) => {
+  return (
+    <>
+      <CaSignup path={path?.path} />
+    </>
+  );
+};
+
+export default aeCaSignup;
---
+++ src/pages/in/ca-signup.js
@@ -0,0 +1,11 @@
+import CaSignup from "@/pages/ca-signup";
+
+const inCaSignup = (path) => {
+  return (
+    <>
+      <CaSignup path={path?.path} />
+    </>
+  );
+};
+
+export default inCaSignup;
